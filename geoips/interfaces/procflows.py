aaeb027b05883687d1da9f9427848668d820b643
mindyls <13873624+mindyls@users.noreply.github.com>
2023-02-14 07:20:02 -0800

    Update RST syntax procflows.py interface docstring

--- geoips/interfaces/procflows.py
+++ geoips/interfaces/procflows.py
@@ -6,11 +6,12 @@ class ProcflowsInterface(BaseInterface):
 
     Proclows drive a specific collection of steps for a particular type of
     processing.  Currently available are:
+    
     * single_source: single input type and single output type.
     * overlay: two input types (one for foreground and one for background),
-    with a single output type.
+      with a single output type.
     * config_based: efficient method for producing all possible outputs for a
-    given set of data files.
+      given set of data files.
     """
 
     name = "procflows"
