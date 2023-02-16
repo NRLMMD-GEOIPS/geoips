25347fdf9f51d4add3290aaa369709cb6659012f
mindyls <mindyls@users.noreply.github.com>
2023-02-16 13:05:21 -0800

    Use original code for deprecated interfaces
    To simplify the new tests, use the original code in test_interfaces for the deprecated
    interfaces, so the "new" interface tests can be clean.
    NRLMMD-GEOIPS/geoips#91 NRLMMD-GEOIPS/geoips#72

--- geoips/commandline/test_interfaces.py
+++ geoips/commandline/test_interfaces.py
@@ -20,79 +20,36 @@ from importlib import import_module
 import traceback
 
 
-def main():
-    """Script to test all dev and stable interfaces."""
-    # Removed all interfaces that have been moved to new setup.
-    # This entire testing construct will be updated in the future to more fully
-    # test/validate the plugins.
+def test_deprecated_interfaces():
+    """Test the "old" deprecated interfaces.
+
+    This function will be removed once all interfaces are moved to the "new" setup.
+    """
+
     interfaces = [
-        "interfaces.algorithms",
         "dev.boundaries",
-        "interfaces.colormaps",
-        "interfaces.filename_formats",
         "dev.gridlines",
-        "interfaces.interpolators",
-        "interfaces.output_formats",
-        "interfaces.procflows",
         "dev.product",
-        "interfaces.readers",
-        "interfaces.title_formats",
     ]
 
-    interface_name_outliers = {'Boundaries': 'Map',
-                               'Colormaps': 'ColorMaps',
-                               'TitleFormats': 'TitleFormatters',
-                               }
-
     for curr_interface in interfaces:
-        #interface_name = curr_interface.split(".")[1]
-        raw_interface_name = curr_interface.split(".")[1]
-
-        interface_name = "".join([name.capitalize() for name in raw_interface_name.split('_')])
-
-        if interface_name in interface_name_outliers:
-            interface_name = interface_name_outliers[interface_name]
-
+        interface_name = curr_interface.split(".")[1]
         print("")
         print(f"Testing {curr_interface}...")
         print("ipython")
-
+        print(
+            f"    from geoips.{curr_interface} import test_{interface_name}_interface"
+        )
+        print(f"    test_{interface_name}_interface()")
+        test_curr_interface = getattr(
+            import_module(f"geoips.{curr_interface}"),
+            f"test_{interface_name}_interface",
+        )
         try:
-            test_curr_interface = getattr(
-                import_module(f"geoips.{curr_interface}"),
-                f"{interface_name}Interface",
-            )
-            print(
-                f"    from geoips.{curr_interface} import {interface_name}Interface"
-            )
-            print(f"    {interface_name}Interface()")
-
-            curr_class = test_curr_interface()
-
-            try:
-                out_dict = curr_class.test_interface_plugins()
-            except Exception:
-                print(traceback.format_exc())
-                raise
-        except AttributeError:
-            interface_name = raw_interface_name
-
-            test_curr_interface = getattr(
-                import_module(f"geoips.{curr_interface}"),
-                f"test_{interface_name}_interface",
-            )
-            print(
-                f"    from geoips.{curr_interface} import test_{interface_name}_interface"
-            )
-            print(f"    test_{interface_name}_interface()")
-
-            try:
-                out_dict = test_curr_interface()
-            except Exception:
-                print(traceback.format_exc())
-                raise
-
-        print(f"SUCCESSFUL INTERFACE {curr_interface}")
+            out_dict = test_curr_interface()
+        except Exception:
+            print(traceback.format_exc())
+            raise
 
         ppprinter = pprint.PrettyPrinter(indent=2)
         ppprinter.pprint(out_dict)
@@ -103,6 +60,65 @@ def main():
                 raise TypeError(
                     f"Failed validity check on {modname} in interface {curr_interface}"
                 )
+
+        print(f"SUCCESSFUL INTERFACE {curr_interface}")
+
+
+def main():
+    """Script to test all dev and stable interfaces."""
+
+    # Test all the "old" interfaces using the original logic.
+    test_deprecated_interfaces()
+
+    interfaces = [
+        "algorithms",
+        "colormaps",
+        "filename_formats",
+        "interpolators",
+        "output_formats",
+        "procflows",
+        "readers",
+        "title_formats",
+    ]
+
+    for curr_interface in interfaces:
+
+        print("")
+        print(f"Testing {curr_interface}...")
+
+        test_curr_interface = getattr(
+            import_module(f"geoips.interfaces"), curr_interface
+        )
+        print(f"    from geoips.interfaces import {curr_interface}")
+
+        # First just use plugins_all_valid, will return True or False
+        all_valid = test_curr_interface.plugins_all_valid()
+        if not all_valid:
+            raise TypeError(
+                f"Failed validity check on interface {curr_interface}",
+                f"plugins_all_valid returned {all_valid}"
+            )
+
+        # Now open all the interfaces (not just checking call signatures)
+        # This returns a dictionary of all sorts of stuff.
+        # If this fails and plugins_all_valid passes, we have a problem.
+        try:
+            out_dict = test_curr_interface.test_interface_plugins()
+        except Exception:
+            print(traceback.format_exc())
+            raise
+
+        ppprinter = pprint.PrettyPrinter(indent=2)
+        ppprinter.pprint(out_dict)
+
+        for modname in out_dict["validity_check"]:
+            if not out_dict["validity_check"][modname]:
+                print(f"FAILED INTERFACE {curr_interface} on {modname}")
+                raise TypeError(
+                    f"Failed validity check on {modname} in interface {curr_interface}"
+                )
+
+        print(f"SUCCESSFUL INTERFACE {curr_interface}")
 
 
 if __name__ == "__main__":
