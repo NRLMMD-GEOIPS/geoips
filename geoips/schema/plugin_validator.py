355955ba3e44b6be26a92526d7748428cfca3463
mindyls <mindyls@users.noreply.github.com>
2023-04-05 13:34:16 -0700

    Update plugin_validator to support subdirectories
    Previously all schema had to be in the top level "schema" directory.
    
    Update plugin_validator to collect schema from subdirectories as well.
    
    Schema will be named as
    <subdir>.<filename_without_extension>
    ie,
    product_templates.alg
    for
    product_temlpates/alg.yaml
    
    In [1]: from geoips.schema.plugin_validator import validate; import yaml; from glob import glob; import jsonschema
    Adding validator boundaries
    Adding validator gridlines
    Adding validator product_templates.alg
    
    In [2]: validate('../interface_modules/product_templates/alg.yaml')
    Out[2]:
    {'interface': 'product_templates.alg',
     'metadata': {'name': 'alg', 'family': 'alg'},
      'spec': {'algorithm': {'name': 'single_channel', 'arguments': {}}}}
    
    GEOIPS/geoips#122

--- geoips/schema/plugin_validator.py
+++ geoips/schema/plugin_validator.py
@@ -33,11 +33,17 @@ DefaultValidatingValidator = extend_with_default(jsonschema.Draft202012Validator
 schema_path = files("geoips.schema")
 # Don't use `schema_path.glob` since if it is a MultiplexedPath it won't have `glob`
 schema_files = glob(str(schema_path / "*.yaml"))
+schema_files += glob(str(schema_path / "*/*.yaml"))
 all_schema = {}
 validators = {}
 for schema_file in schema_files:
-    schema_name = os.path.splitext(os.path.basename(schema_file))[0]
+    schema_name = (
+        schema_file.replace(str(schema_path) + "/", "")
+        .replace("/", ".")
+        .replace(".yaml", "")
+    )
     schema = yaml.safe_load(open(schema_file, "r"))
+    print(f"Adding validator {schema_name}")
     DefaultValidatingValidator.check_schema(schema)
     all_schema[schema_name] = schema
     validators[schema_name] = DefaultValidatingValidator(schema)
