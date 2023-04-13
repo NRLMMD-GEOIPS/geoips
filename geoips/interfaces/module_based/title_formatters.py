7069186e613d6e9694cb44e1f44449c982a53993
mindyls <13873624+mindyls@users.noreply.github.com>
2023-04-13 15:51:24 -0700

    Update title_formatters.py interfacename

--- geoips/interfaces/module_based/title_formatters.py
+++ geoips/interfaces/module_based/title_formatters.py
@@ -10,7 +10,7 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-"""Title formats interface module."""
+"""Title formatters interface module."""
 
 from geoips.interfaces.base import BaseInterface, BasePlugin
 
@@ -18,11 +18,11 @@ from geoips.interfaces.base import BaseInterface, BasePlugin
 class TitleFormattersInterface(BaseInterface):
     """Interface for creating GeoIPS formatted titles."""
 
-    name = "title_formats"
+    name = "title_formatters"
     entry_point_group = "title_formats"
     deprecated_family_attr = "title_type"
     required_args = {"standard": []}
     required_kwargs = {"standard": []}
 
 
-title_formats = TitleFormattersInterface()
+title_formatters = TitleFormattersInterface()
