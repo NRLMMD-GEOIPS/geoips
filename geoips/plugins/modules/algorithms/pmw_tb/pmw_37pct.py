f5b9803ceb578321103f1ab613b9d8baebe6ff23
mindyls <13873624+mindyls@users.noreply.github.com>
2023-04-28 16:10:48 -0700

    Update pmw_37pct.py

--- geoips/plugins/modules/algorithms/pmw_tb/pmw_37pct.py
+++ geoips/plugins/modules/algorithms/pmw_tb/pmw_37pct.py
@@ -10,7 +10,7 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-"""Passive Microwave 37 MHz Polarization Corrected Temperature.
+"""Passive Microwave 37 GHz Polarization Corrected Temperature.
 
 Data manipulation steps for the "37pct" product.
 This algorithm expects Brightness Temperatures in units of degrees Kelvin.
