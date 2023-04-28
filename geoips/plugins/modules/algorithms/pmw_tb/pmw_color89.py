0e35f2045ea673ded72df5595e688b2f50a8c454
mindyls <13873624+mindyls@users.noreply.github.com>
2023-04-28 16:18:12 -0700

    Update pmw_color89.py GHz

--- geoips/plugins/modules/algorithms/pmw_tb/pmw_color89.py
+++ geoips/plugins/modules/algorithms/pmw_tb/pmw_color89.py
@@ -10,7 +10,7 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-"""Passive Microwave 89 MHz Colorized Brightness Temperature.
+"""Passive Microwave 89 GHz Colorized Brightness Temperature.
 
 Data manipulation steps for the "color89" product.
 This algorithm expects Brightness Temperatures in units of degrees Kelvin
