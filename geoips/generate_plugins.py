130b6c1abc6fe02318b02ef1a9ff3155fc256dcf
evrose54 <mrewrose@gmail.com>
2023-07-06 10:00:56 -0600

    updated generate_plugins.py to be flexible with new plugins/interfaces created

--- geoips/generate_plugins.py
+++ geoips/generate_plugins.py
@@ -1,32 +1,9 @@
 from glob import glob
 import yaml
-import sys
 
-plugins = {"feature_annotators": {},
-           "gridline_annotators": {},
-           "product_defaults": {},
-           "products": {},
-           "sectors": {},
-           "bases": {},
-           "algorithms": {},
-           "colormappers": {},
-           "colormaps": {},
-           "coverage_checkers": {},
-           "filename_formatters": {},
-           "interpolators": {},
-           "output_formatters": {},
-           "procflows": {},
-           "readers": {},
-           "sector_metadata_generators": {},
-           "sector_spec_generators": {},
-           "title_formatters": {},
-           }
+plugins = {}
 
-plugin_paths = {"plugins.yaml.feature_annotators": glob(".plugins/yaml/feature_annotators/*.yaml"),
-                "plugins.yaml.gridline_annotators": glob("./plugins/yaml/gridline_annotators/*.yaml"),
-                "plugins.yaml.product_defaults": glob("./plugins/yaml/product_defaults/**/*.yaml", recursive=True),
-                "plugins.yaml.products": glob("./plugins/yaml/products/*.yaml"),
-                "plugins.yaml.sectors": glob("./plugins/yaml/sectors/**/*.yaml", recursive=True),
+plugin_paths = {"plugins.yamls": glob("./plugins/yaml/**/*.yaml", recursive=True),
                 "plugins.modules.algorithms": glob("./plugins/modules/algorithms/**/*.py", recursive=True),
                 "plugins.modules.colormappers": glob("./plugins/modules/colormappers/**/*.py", recursive=True),
                 "plugins.modules.colormaps": glob("./plugins/modules/colormaps/**/*.py", recursive=True),
@@ -46,37 +23,44 @@ plugin_paths = {"plugins.yaml.feature_annotators": glob(".plugins/yaml/feature_a
                 "schema.products": glob("./schema/products/**/*.yaml", recursive=True),
                 "schema.sectors": glob("./schema/sectors/*.yaml"),
                 }
+
 def main():
     for interface_key in plugin_paths:
-        interface_name = interface_key.split(".")[-1]
         for filepath in plugin_paths[interface_key]:
             if interface_key.split(".")[0] == "plugins" and filepath[-4:] == "yaml":
                 plugin = yaml.safe_load(open(filepath, mode="r"))
+                interface_name = plugin["interface"]
+                if interface_name not in plugins.keys():
+                    plugins[interface_name] = {}
                 name = plugin["name"]
                 family = plugin["family"]
                 docstring = plugin["docstring"]
                 plugins[interface_name][name] = {"family": family, "docstring": docstring}
-            elif filepath[-4:] == "yaml": #schema yaml files
-                plugin = yaml.safe_load(open(filepath, mode="r"))
-                id = plugin["$id"]
-                yaml_type = None
-                description = None
-                try:
-                    description = plugin["description"]
-                    yaml_type = plugin["type"]
-                    plugins[interface_name][id] = {"type": yaml_type, "description": description}
-                except Exception:
-                    plugins[interface_name][id] = {"type": yaml_type, "description": description}
             else:
-                if "__init__.py" in filepath: continue
-                import_str = "from geoips.plugins.modules." + interface_name
-                entry_points = filepath.split("/")
-                if interface_name == entry_points[-2]:
-                    import_str += " import " + entry_points[-1][0:-3]
+                interface_name = interface_key.split(".")[-1]
+                if interface_name not in plugins.keys():
+                    plugins[interface_name] = {}
+                if filepath[-4:] == "yaml": #schema yaml files
+                    plugin = yaml.safe_load(open(filepath, mode="r"))
+                    id = plugin["$id"]
+                    yaml_type = None
+                    description = None
+                    try:
+                        description = plugin["description"]
+                        yaml_type = plugin["type"]
+                        plugins[interface_name][id] = {"type": yaml_type, "description": description}
+                    except Exception:
+                        plugins[interface_name][id] = {"type": yaml_type, "description": description}
                 else:
-                    import_str += "." + entry_points[-2] + " import " + entry_points[-1][0:-3]
-                plugins[interface_name][entry_points[-1][0:-3]] = {"import_string": import_str}
-    print(plugins)
+                    if "__init__.py" in filepath: continue
+                    import_str = "from geoips.plugins.modules." + interface_name
+                    entry_points = filepath.split("/")
+                    if interface_name == entry_points[-2]:
+                        import_str += " import " + entry_points[-1][0:-3]
+                    else:
+                        import_str += "." + entry_points[-2] + " import " + entry_points[-1][0:-3]
+                    plugins[interface_name][entry_points[-1][0:-3]] = {"import_string": import_str}
+    print("Avalable plugin keys:\n" + str(plugins.keys()))
     return plugins
 
 if __name__ == "__main__":
