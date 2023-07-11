165364e398b16fe42d791a51d07b93c553d72f40
evrose54 <mrewrose@gmail.com>
2023-07-11 09:33:51 -0600

    updated create_plugin_registry.py to work with additional GeoIPS packages

--- geoips/create_plugin_registry.py
+++ geoips/create_plugin_registry.py
@@ -1,17 +1,13 @@
 from glob import glob
 import yaml
 import numpy as np
-import importlib
-from importlib import metadata
+from importlib import metadata, resources, util
 import os
 import sys
 
 plugins = {}
 
-plugin_paths = {"plugins.yamls": glob("./plugins/yaml/**/*.yaml", recursive=True),
-                "plugins.modules": glob("./plugins/modules/**/*.py", recursive=True),
-                "schema.yamls": glob("./schema/**/*.yaml", recursive=True),
-                }
+schema_yamls = glob(os.getcwd() + "/schema/**/*.yaml", recursive=True)
 
 def get_entry_point_group(group):
     """Get entry point group."""
@@ -22,52 +18,57 @@ def get_entry_point_group(group):
 
 def main():
     plugin_packages = get_entry_point_group("geoips.plugin_packages")
-    package = plugin_packages[0].value
-    for interface_key in plugin_paths:
-        for filepath in plugin_paths[interface_key]:
-            abspath = os.path.abspath(filepath)
-            relpath = os.path.relpath(filepath)
-            if interface_key.split(".")[0] == "plugins" and interface_key.split(".")[-1] == "yamls":
-                plugin = yaml.safe_load(open(filepath, mode="r"))
-                interface_name = plugin["interface"]
-                if interface_name not in plugins.keys():
-                    plugins[interface_name] = []
-                plugin["abspath"] = abspath
-                plugin["relpath"] = relpath; plugin["package"] = package
-                plugins[interface_name].append(plugin)
-            else:
-                if interface_key.split(".")[0] == "schema": #schema yaml files
-                    split_path = np.array(filepath.split("/"))
-                    interface_idx = np.argmax(split_path == "schema") + 1
-                    interface_name = split_path[interface_idx]
+    print(plugin_packages)
+    for pkg in plugin_packages:
+        package = pkg.value
+        print("package == " + str(package))
+        pkg_plugin_path = resources.files(package) / "plugins"
+        yaml_files = pkg_plugin_path.rglob("*.yaml")
+        python_files = pkg_plugin_path.rglob("*.py")
+        schemas = [] if package != "geoips" else schema_yamls
+        plugin_paths = {"yamls": yaml_files, "schemas": schemas, "pyfiles": python_files}
+        for interface_key in plugin_paths:
+            for filepath in plugin_paths[interface_key]:
+                filepath = str(filepath)
+                abspath = os.path.abspath(filepath)
+                relpath = os.path.relpath(filepath)
+                if interface_key == "yamls": # yaml based plugins
+                    plugin = yaml.safe_load(open(filepath, mode="r"))
+                    interface_name = plugin["interface"]
                     if interface_name not in plugins.keys():
                         plugins[interface_name] = []
-                    plugin = yaml.safe_load(open(filepath, mode="r"))
                     plugin["abspath"] = abspath
                     plugin["relpath"] = relpath; plugin["package"] = package
                     plugins[interface_name].append(plugin)
                 else:
-                    if "__init__.py" in filepath: continue
-                    split_path = np.array(filepath.split("/"))
-                    interface_idx = np.argmax(split_path == "modules") + 1
-                    import_str = "plugins.modules"
-                    for folder in split_path[interface_idx: -1]:
-                        import_str += "." + folder
-                    filename = split_path[-1][0:-3]
-                    import_str += "." + filename
-                    try:
-                        module = importlib.import_module(import_str)
-                        interface_name = module.interface
+                    if interface_key == "schemas": #schema based yamls
+                        split_path = np.array(filepath.split("/"))
+                        interface_idx = np.argmax(split_path == "schema") + 1
+                        interface_name = split_path[interface_idx]
                         if interface_name not in plugins.keys():
                             plugins[interface_name] = []
-                        family = module.family
-                        name = module.name
-                        del module
-                        module_plugin = {name: {"interface": interface_name, "family": family, "name": name, 
-                                                            "abspath": abspath, "relpath": relpath, "package": package}}
-                        plugins[interface_name].append(module_plugin)
-                    except Exception as e:
-                        continue
+                        plugin = yaml.safe_load(open(filepath, mode="r"))
+                        plugin["abspath"] = abspath
+                        plugin["relpath"] = relpath; plugin["package"] = package
+                        plugins[interface_name].append(plugin)
+                    else: # module based plugins
+                        if "__init__.py" in abspath: continue
+                        module_name = str(abspath).split("/")[-1][:-3]
+                        spec = util.spec_from_file_location(module_name, abspath)
+                        try:
+                            module = util.module_from_spec(spec)
+                            spec.loader.exec_module(module)
+                            interface_name = module.interface
+                            if interface_name not in plugins.keys():
+                                plugins[interface_name] = []
+                            family = module.family
+                            name = module.name
+                            del module
+                            module_plugin = {name: {"interface": interface_name, "family": family, "name": name, 
+                                                                "abspath": abspath, "relpath": relpath, "package": package}}
+                            plugins[interface_name].append(module_plugin)
+                        except:
+                            continue
     print("Available Plugin Interfaces:\n" + str(plugins.keys()))
     with open("registered_plugins.py", "w") as plugin_registry:
         plugin_registry.write("registered_plugins = {}".format(plugins))
