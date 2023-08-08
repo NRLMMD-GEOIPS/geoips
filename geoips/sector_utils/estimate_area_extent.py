87c007c5d37b4b69c38b5a4c4aa58c447ccd05fd
evrose54 <mrewrose@gmail.com>
2023-08-08 14:41:49 -0600

    updated estimate_area_extent.py to allow center-based area_definitions

--- geoips/sector_utils/estimate_area_extent.py
+++ geoips/sector_utils/estimate_area_extent.py
@@ -19,6 +19,36 @@ from string import Template
 EARTH_RADIUS_METERS = 6371228.0
 
 
+def generateMinMaxLatLong(lat_0, lon_0, height, width, resolution):
+    """Generate minimum and maximum latitude longitude pairs based off the resolution 
+        and height/width provided
+
+    Parameters
+    ----------
+    lat_0, lon_0 : float
+        Pair of latitude and longitude coordinates in degrees
+    
+    height, width, resolution : int
+        Represents pixel dimensions and resolution of image in meters
+
+    Returns
+    -------
+    list of floats
+        min_lat, max_lat, min_lon, max_lon
+    """
+    phi_1 = np.deg2rad(lat_0)
+    phi_2 = np.deg2rad(lon_0)
+    lat_distance = height * resolution
+    lon_distance = width * resolution
+    lat_rad_dist = lat_distance / EARTH_RADIUS_METERS
+    lon_rad_dist = lon_distance / EARTH_RADIUS_METERS
+    min_lat = np.rad2deg(phi_1 - lat_rad_dist)
+    max_lat = np.rad2deg(phi_1 + lat_rad_dist)
+    delta_lon = np.arcsin(np.sin(lon_rad_dist) / np.cos(phi_1))
+    min_lon = np.rad2deg(phi_2 - delta_lon)
+    max_lon = np.rad2deg(phi_2 + delta_lon)
+    return [min_lat, max_lat, min_lon, max_lon]
+
 def haversine_distance(lat1, lon1, lat2, lon2):
     """Calculate the distance between two latitude and longitude points.
 
@@ -158,6 +188,62 @@ def estimate_area_extent(min_lat, min_lon, max_lat, max_lon, resolution):
         "lon_0": center_lon,
     }
 
+def esitmate_area_from_center(lat_0, lon_0, height, width, resolution):
+    """Estimate the area extent for use in the YAML area definition.
+
+    Parameters
+    ----------
+    lat_0, lon_0 : float
+        Center lat/lon values in degrees
+    height, width : int
+        Pixel dimensions
+    resolution : int
+        Resolution in meters
+
+    Returns
+    -------
+    dict
+        Dictionary holding:
+
+        * lower_left_xy - list of projection x/y coordinates of
+          lower left corner of lower left pixel
+        * upper_right_xy - list of projection x/y coordinates of
+          upper right corner of upper right pixel
+        * height - number of grid rows
+        * width - number of grid columns
+        * lat_0 - Center latitude in degrees
+        * lon_0 - Center longitude in degrees
+    """
+    # Convert all longitude points to degrees east
+    center_lon = convert_west2east(lon_0)
+    center_lat = lat_0
+    lats_lons = generateMinMaxLatLong(lat_0, lon_0, height, width, resolution)
+    max_lonE = convert_west2east(lats_lons[3])
+    min_lonE = convert_west2east(lats_lons[2])
+    lat_distance = haversine_distance(lats_lons[0], center_lon, lats_lons[1], center_lon)
+    # distance = inverse_haversine_distance(lat_0, center_lon, height, width, resolution)
+    if (max_lonE - min_lonE) > 180:
+        dist1 = haversine_distance(center_lat, min_lonE, center_lat, min_lonE + 180)
+        dist2 = haversine_distance(center_lat, min_lonE + 180, center_lat, max_lonE)
+        lon_distance = dist1 + dist2
+    else:
+        lon_distance = haversine_distance(center_lat, lats_lons[2], center_lat, lats_lons[3])
+    # The haversine formula will find the shortest distance between two points
+    # If we want the sector larger than 180 degrees in longitude, we need
+    # some special handling for the distance calculation
+    extent_lat = int(lat_distance / 2.0)
+    extent_lon = int(lon_distance / 2.0)
+    lower_left_xy = [-1 * extent_lon, -1 * extent_lat]
+    upper_right_xy = [extent_lon, extent_lat]
+    return {
+        "lower_left_xy": lower_left_xy,
+        "upper_right_xy": upper_right_xy,
+        "height": height,
+        "width": width,
+        "lat_0": lat_0,
+        "lon_0": lon_0,
+    }
+
 
 AREA_DEF_TEMPLATE = """
 # Copy and paste the following into a yaml sector file.
@@ -186,17 +272,29 @@ AREA_DEF_TEMPLATE = """
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_lat", type=float, required=True)
-    parser.add_argument("--max_lon", type=float, required=True)
-    parser.add_argument("--min_lat", type=float, required=True)
-    parser.add_argument("--min_lon", type=float, required=True)
-    parser.add_argument("--resolution", "-r", type=int, required=True)
+    subparser = parser.add_subparsers(help="sub-parser help")
+    parser_c = subparser.add_parser("c", help="center help")
+    parser_e = subparser.add_parser("e", help="extent help")
+    parser_e.add_argument("--max_lat", type=float, required=True)
+    parser_e.add_argument("--max_lon", type=float, required=True)
+    parser_e.add_argument("--min_lat", type=float, required=True)
+    parser_e.add_argument("--min_lon", type=float, required=True)
+    parser_e.add_argument("--resolution", "-r", type=int, required=True)
+    parser_c.add_argument("--lat_0", type=float, required=True)
+    parser_c.add_argument("--lon_0", type=float, required=True)
+    parser_c.add_argument("--height", type=int, required=True)
+    parser_c.add_argument("--width", "-w", type=int, required=True)
+    parser_c.add_argument("--resolution", "-r", type=int, required=True)
 
     ARGS = parser.parse_args()
-
-    estimated_extent = estimate_area_extent(
-        ARGS.min_lat, ARGS.min_lon, ARGS.max_lat, ARGS.max_lon, ARGS.resolution
-    )
+    try:
+        estimated_extent = estimate_area_extent(
+            ARGS.min_lat, ARGS.min_lon, ARGS.max_lat, ARGS.max_lon, ARGS.resolution
+        )
+    except:
+        estimated_extent = esitmate_area_from_center(
+            ARGS.lat_0, ARGS.lon_0, ARGS.height, ARGS.width, ARGS.resolution
+        )
     lat_0 = estimated_extent["lat_0"]
     lon_0 = estimated_extent["lon_0"]
     height, width = estimated_extent["height"], estimated_extent["width"]
