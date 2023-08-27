5507bdc454f0cb564d6ededc3f457f472bf4f68c
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2023-08-27 10:21:51 -0600

    Comment out area_def functions
    Comment out the new area_definition functions. These work well, but
    should probably be sector_spec_generators rather than built directly
    into the sector module.

--- geoips/interfaces/yaml_based/sectors.py
+++ geoips/interfaces/yaml_based/sectors.py
@@ -19,41 +19,44 @@ from cartopy import feature as cfeature
 from pyresample import create_area_def
 
 
-def center_to_area_definition(sector):
-    """Return a pyresample AreaDefinition for the input sector.
-
-    The input sector must supply location information in the "center" format.
-    """
-    if not sector.family.startswith("center"):
-        raise ValueError("Sector does not supply location as center coordinates.")
-    raise NotImplementedError
-
-
-def corners_to_area_definition(sector):
-    """Return a pyresample AreaDefinition for the input sector.
-
-    The input sector must supply location information in the "corners" format.
-    """
-    if not sector.family.startswith("center"):
-        raise ValueError("Sector does not supply location as corner coordinates.")
-
-    ad_info = {
-        "area_id": sector.name,
-        "projection": {
-            "units": "m",
-            "a": 6371228.0,
-            "proj": sector.spec.projection,
-            "lat_0": sector.spec.center.lat,
-            "lon_0": sector.spec.center.lon,
-        },
-        "width": sector.shape[0],
-        "height": sector.shape[1],
-        "resolution": [sector.resolution, sector.resolution],
-        "center": [0, 0],
-    }
-
-    ad = create_area_def(**ad_info)
-    raise NotImplementedError
+# Commenting these out for PR #260
+# Will work on this again after the 2023 workshop
+#
+# def center_to_area_definition(sector):
+#     """Return a pyresample AreaDefinition for the input sector.
+#
+#     The input sector must supply location information in the "center" format.
+#     """
+#     if not sector.family.startswith("center"):
+#         raise ValueError("Sector does not supply location as center coordinates.")
+#     raise NotImplementedError
+#
+#
+# def corners_to_area_definition(sector):
+#     """Return a pyresample AreaDefinition for the input sector.
+#
+#     The input sector must supply location information in the "corners" format.
+#     """
+#     if not sector.family.startswith("center"):
+#         raise ValueError("Sector does not supply location as corner coordinates.")
+#
+#     ad_info = {
+#         "area_id": sector.name,
+#         "projection": {
+#             "units": "m",
+#             "a": 6371228.0,
+#             "proj": sector.spec.projection,
+#             "lat_0": sector.spec.center.lat,
+#             "lon_0": sector.spec.center.lon,
+#         },
+#         "width": sector.shape[0],
+#         "height": sector.shape[1],
+#         "resolution": [sector.resolution, sector.resolution],
+#         "center": [0, 0],
+#     }
+#
+#     ad = create_area_def(**ad_info)
+#     raise NotImplementedError
 
 
 class SectorPluginBase(BaseYamlPlugin):
@@ -67,15 +70,17 @@ class SectorPluginBase(BaseYamlPlugin):
     @property
     def area_definition(self):
         """Return the pyresample AreaDefinition for the sector."""
+        # if self.family.startswith(("area_definition", "generated")):
         if self.family.startswith("area_definition"):
             ad = load_area(self.abspath, "spec")
-        elif self.family.startswith("center"):
-            ad = center_to_area_definition(self)
-        elif self.family.startswith("corners"):
-            ad = corners_to_area_definition(self)
+        # elif self.family.startswith("center"):
+        #     ad = center_to_area_definition(self)
+        # elif self.family.startswith("corners"):
+        #     ad = corners_to_area_definition(self)
         return ad
 
     def create_test_plot(self, fname):
+        """Create a test PNG image for this sector."""
         fig, ax, mapobj = create_figure_and_main_ax_and_mapobj(
             self.area_definition.shape[1],
             self.area_definition.shape[0],
