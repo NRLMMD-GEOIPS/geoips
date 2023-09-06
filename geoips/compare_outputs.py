817a4bf815a723c9616f302019f308e7bd194426
evrose54 <mrewrose@gmail.com>
2023-09-06 10:35:58 -0600

    comparing using np.abs function

--- geoips/compare_outputs.py
+++ geoips/compare_outputs.py
@@ -226,7 +226,7 @@ def images_match(output_product, compare_product, fuzz="5%"):
     out_img = Image.open(output_product)
     comp_img = Image.open(compare_product)
     diff_img = Image.new(mode="RGB", size=comp_img.size)
-    diff_arr = np.asarray(comp_img) - np.asarray(out_img)
+    diff_arr = np.abs(np.array(comp_img) - np.array(out_img))
     num_pix_mismatched = pixelmatch(out_img, comp_img, diff_img, includeAA=True,
                                     alpha=0.33, threshold=0.05)
     fullimg_retval = 0 if np.all(diff_arr == 0) and num_pix_mismatched == 0 else 1
