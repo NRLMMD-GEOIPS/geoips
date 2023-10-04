7345dda6d8e212b7d2a8bebf36fd86a7af4af6af
Andrew Thorpe <96952160+AJThorpe@users.noreply.github.com>
2023-10-04 17:29:55 -0400

    Conversion of xarray to datatree for future use
    * Added xarray functionality

---
+++ geoips/xarray_utils/xr_to_dtree.py
@@ -0,0 +1,27 @@
+
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+"""Takes in a dictionary of xarrays and converts to xarray datatree"""
+
+
+import logging
+from datatree import DataTree
+
+LOG = logging.getLogger(__name__)
+
+def xarray_to_datatree(xarray_dict):
+    
+    xarray_datatree = DataTree.from_dict(xarray_dict)
+    assert xarray_datatree == xarray_dict
+
+    return xarray_datatree
