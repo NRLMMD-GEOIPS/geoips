6cb22e0ea1cf4ed647419fa5b0cae9513ab5e7a8
Andrew Thorpe <96952160+AJThorpe@users.noreply.github.com>
2023-10-13 13:39:18 -0400

    Updated unit tests for reader
    * Added more paths for certain readers
    * Added xfail flag for readers without files

--- tests/test_pytest/reader_test.py
+++ tests/test_pytest/reader_test.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+"""Tests all the readers given input test files."""
 
 import pytest
 from geoips.commandline.log_setup import setup_logging
@@ -6,8 +18,6 @@ from geoips.interfaces import readers
 from glob import glob
 
 LOG = setup_logging()
-
-# @pytest.fixture
 
 
 @pytest.mark.parametrize(
@@ -30,8 +40,11 @@ LOG = setup_logging()
         ("modis_hdf4", ""),
         ("saphir_hdf5", ""),
         ("sar_winds_netcdf", "geoips/test_data/test_data_sar/data/*.nc"),
-        ("scat_knmi_winds_netcdf", ""),
-        ("scat_noaa_winds_netcdf", ""),
+        ("scat_knmi_winds_netcdf", "geoips/test_data/test_data_scat/data/20220*/*.nc"),
+        (
+            "scat_noaa_winds_netcdf",
+            "geoips/test_data/test_data_scat/data/20230524_metopc_noaa*/*.nc",
+        ),
         ("seviri_hrit", ""),
         ("sfc_winds_text", ""),
         ("smap_remss_winds_netcdf", "geoips/test_data/test_data_smap/data/*.nc"),
@@ -45,8 +58,9 @@ LOG = setup_logging()
     ],
 )
 def test_standards(key, files):
-    """Takes the input xarray and tests for conformity with internal standards"""
-    if 
+    """Takes the input xarray and tests for conformity with internal standards."""
+    if len(glob(files)) == 0:
+        pytest.xfail("No files given")
     tmp_reader = readers.get_plugin(key)
     inxr = tmp_reader(glob(files)[:2])  # sometimes get core dumps?
     assert inxr  # how would we test the data key??
