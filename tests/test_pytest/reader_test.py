04a38134c875c71aa64f3abaf633ff655ef477e5
Andrew Thorpe <96952160+AJThorpe@users.noreply.github.com>
2023-10-16 19:54:03 -0400

    Updated testing
    * Added extra testing of dataset values (lat/lon)
    * Fixed testing of data dirs with environmental pathing

--- tests/test_pytest/reader_test.py
+++ tests/test_pytest/reader_test.py
@@ -12,61 +12,78 @@
 
 """Tests all the readers given input test files."""
 
-import pytest
+import os
 from glob import glob
+import pytest
 from geoips.commandline.log_setup import setup_logging
 from geoips.interfaces import readers
+
 
 LOG = setup_logging()
 
 
 @pytest.mark.parametrize(
-    "key,files",
+    "key,files,data_key",
     [
-        ("abi_l2_netcdf", ""),
-        ("abi_netcdf", ""),
-        ("ahi_hsd", ""),
-        ("amsr2_netcdf", "geoips/test_data/test_data_amsr2/data/AMSR2*.nc"),
-        ("amsr2_remss_winds_netcdf", "geoips/test_data/test_data_amsr2/data/RSS*.nc"),
-        ("amsub_hdf", ""),
-        ("amsub_mirs", ""),
-        ("ascat_uhr_netcdf", ""),
-        ("atms_hdf5", ""),
-        ("ewsg_netcdf", ""),
-        ("geoips_netcdf", ""),
-        ("gmi_hdf5", "geoips/test_data/test_data_gpm/data/*.RT-H5"),
-        ("imerg_df5", ""),
-        ("mimic_netcdf", ""),
-        ("modis_hdf4", ""),
-        ("saphir_hdf5", ""),
-        ("sar_winds_netcdf", "geoips/test_data/test_data_sar/data/*.nc"),
+        ("abi_l2_netcdf", "", ""),
+        ("abi_netcdf", "", ""),
+        ("ahi_hsd", "", ""),
+        (
+            "amsr2_netcdf",
+            "/test_data_amsr2/data/AMSR2*.nc",
+            "Brightness_Temperature_10_GHzH",
+        ),
+        ("amsr2_remss_winds_netcdf", "/test_data_amsr2/data/RSS*.nc", "WINDSPEED_1"),
+        ("amsub_hdf", "", ""),
+        ("amsub_mirs", "", ""),
+        ("ascat_uhr_netcdf", "", ""),
+        ("atms_hdf5", "", ""),
+        ("ewsg_netcdf", "", ""),
+        ("geoips_netcdf", "", ""),
+        ("gmi_hdf5", "/test_data_gpm/data/*.RT-H5", "GMI"),
+        ("imerg_df5", "", ""),
+        ("mimic_netcdf", "", ""),
+        ("modis_hdf4", "", ""),
+        ("saphir_hdf5", "", ""),
+        ("sar_winds_netcdf", "/test_data_sar/data/*.nc", "WINDSPEED"),
         (
             "scat_knmi_winds_netcdf",
-            "geoips/test_data/test_data_scat/data/metopc*knmi*/*coa*.nc",
+            "/test_data_scat/data/metopc*knmi*/*coa*.nc",
+            "WINDSPEED",
         ),
         (
             "scat_noaa_winds_netcdf",
-            "geoips/test_data/test_data_scat/data/20230524_metopc_noaa*/*.nc",
+            "/test_data_scat/data/20230524_metopc_noaa*/*.nc",
+            "WINDSPEED",
         ),
-        ("seviri_hrit", ""),
-        ("sfc_winds_text", ""),
-        ("smap_remss_winds_netcdf", "geoips/test_data/test_data_smap/data/*.nc"),
-        ("smos_winds_netcdf", ""),
-        ("ssmi_binary", ""),
-        ("ssmis_binary", ""),
-        ("viirs_netcdf", ""),
-        ("wfabba_ascii", ""),
-        ("windsat_idr37_binary", ""),
-        ("windsat_remss_winds_netcdf", ""),
+        ("seviri_hrit", "", ""),
+        ("sfc_winds_text", "", ""),
+        ("smap_remss_winds_netcdf", "/test_data_smap/data/*.nc", "WINDSPEED_1"),
+        ("smos_winds_netcdf", "", ""),
+        ("ssmi_binary", "", ""),
+        ("ssmis_binary", "", ""),
+        ("viirs_netcdf", "", ""),
+        ("wfabba_ascii", "", ""),
+        ("windsat_idr37_binary", "", ""),
+        ("windsat_remss_winds_netcdf", "", ""),
     ],
 )
-def test_standards(key, files):
+def test_standards(key, files, data_key):
     """Takes the input xarray and tests for conformity with internal standards."""
-    if len(glob(files)) == 0:
-        pytest.xfail("No files given")
+    wc_path = os.environ["GEOIPS_TESTDATA_DIR"] + files
+    filelist = glob(wc_path)
+
+    if len(filelist) == 0 or os.path.isdir(filelist[0]):
+        pytest.xfail("No files given, or bad path")
     tmp_reader = readers.get_plugin(key)
-    inxr = tmp_reader(glob(files)[:2])  # sometimes get core dumps?
-    assert inxr  # how would we test the data key??
+    inxr = tmp_reader(filelist[:2])
+
+    assert inxr
+    assert inxr[data_key]
+    assert inxr[data_key].longitude.max()
+    assert inxr[data_key].longitude.min()
+    assert inxr[data_key].latitude.max()
+    assert inxr[data_key].latitude.min()
     assert inxr["METADATA"].attrs["source_name"]
     assert inxr["METADATA"].attrs["platform_name"]
     assert inxr["METADATA"].attrs["data_provider"]
