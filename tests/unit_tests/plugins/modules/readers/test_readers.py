a3df1771a64016a42a6592456dc8d1380ca271f9
Andrew Thorpe <96952160+AJThorpe@users.noreply.github.com>
2023-10-23 12:00:43 -0400

    Updated reader unit test format
    * Added plugin checking format for reader unit testing

--- tests/unit_tests/plugins/modules/readers/test_readers.py
+++ tests/unit_tests/plugins/modules/readers/test_readers.py
@@ -11,9 +11,6 @@
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
 """Unit tests on all the readers."""
-
-import os
-from glob import glob
 import pytest
 from geoips.commandline.log_setup import setup_logging
 from geoips.interfaces import readers
@@ -23,55 +20,50 @@ LOG = setup_logging()
 
 
 class TestReaders:
-    """Tests all reader, checking GEOIPS standards and keywords."""
+    """Unit tests every reader."""
 
-    data_dir = os.environ["GEOIPS_TESTDATA_DIR"]
     available_readers = [
-        ("abi_l2_netcdf", "", ""),
-        ("abi_netcdf", "", ""),
-        ("ahi_hsd", "", ""),
-        (
-            "amsr2_netcdf",
-            "/test_data_amsr2/data/AMSR2*.nc",
-            "Brightness_Temperature_10_GHzH",
-        ),
-        ("amsr2_remss_winds_netcdf", "/test_data_amsr2/data/RSS*.nc", "WINDSPEED_1"),
-        ("amsub_hdf", "", ""),
-        ("amsub_mirs", "", ""),
-        ("ascat_uhr_netcdf", "", ""),
-        ("atms_hdf5", "", ""),
-        ("ewsg_netcdf", "", ""),
-        ("geoips_netcdf", "", ""),
-        ("gmi_hdf5", "/test_data_gpm/data/*.RT-H5", "GMI"),
-        ("imerg_df5", "", ""),
-        ("mimic_netcdf", "", ""),
-        ("modis_hdf4", "", ""),
-        ("saphir_hdf5", "", ""),
-        ("sar_winds_netcdf", "/test_data_sar/data/*.nc", "WINDSPEED"),
-        (
-            "scat_knmi_winds_netcdf",
-            "/test_data_scat/data/metopc*knmi*/*coa*.nc",
-            "WINDSPEED",
-        ),
-        (
-            "scat_noaa_winds_netcdf",
-            "/test_data_scat/data/20230524_metopc_noaa*/*.nc",
-            "WINDSPEED",
-        ),
-        ("seviri_hrit", "", ""),
-        ("sfc_winds_text", "", ""),
-        ("smap_remss_winds_netcdf", "/test_data_smap/data/*.nc", "WINDSPEED_1"),
-        ("smos_winds_netcdf", "", ""),
-        ("ssmi_binary", "", ""),
-        ("ssmis_binary", "", ""),
-        ("viirs_netcdf", "", ""),
-        ("wfabba_ascii", "", ""),
-        ("windsat_idr37_binary", "", ""),
-        ("windsat_remss_winds_netcdf", "", ""),
+        ("abi_l2_netcdf"),
+        ("abi_netcdf"),
+        ("ahi_hsd"),
+        ("amsr2_netcdf"),
+        ("amsr2_remss_winds_netcdf"),
+        ("amsub_hdf"),
+        ("amsub_mirs"),
+        ("ascat_uhr_netcdf"),
+        ("atms_hdf5"),
+        ("ewsg_netcdf"),
+        ("geoips_netcdf"),
+        ("gmi_hdf5"),
+        ("imerg_hdf5"),
+        ("mimic_netcdf"),
+        ("modis_hdf4"),
+        ("saphir_hdf5"),
+        ("sar_winds_netcdf"),
+        ("scat_knmi_winds_netcdf"),
+        ("scat_noaa_winds_netcdf"),
+        ("seviri_hrit"),
+        ("sfc_winds_text"),
+        ("smap_remss_winds_netcdf"),
+        ("smos_winds_netcdf"),
+        ("ssmi_binary"),
+        ("ssmis_binary"),
+        ("viirs_netcdf"),
+        ("wfabba_ascii"),
+        ("windsat_idr37_binary"),
+        ("windsat_remss_winds_netcdf"),
     ]
 
-    def check_xr(self, inxr, data_key):
-        """Check the input array with GEOIPS xarray standards."""
+    def verify_plugin(self, plugin):
+        """Yeild test xarray and parameters."""
+        test_xr = plugin.module.yeild_test_files()
+        test_param = plugin.module.yeild_test_parameters()
+        self.verify_xarray(test_xr, test_param)
+
+    def verify_xarray(self, inxr, test_parameters):
+        """Test every parameter."""
+        # could add more parameters; lon_max, lat_max, etc
+        data_key = test_parameters
         assert inxr
         assert inxr[data_key]
         assert inxr[data_key].longitude.max()
@@ -85,28 +77,12 @@ class TestReaders:
         assert inxr["METADATA"].attrs["end_datetime"]
         assert inxr["METADATA"].attrs["interpolation_radius_of_influence"]
 
-    @pytest.mark.parametrize("key,files,data_key", available_readers)
-    def test_reader_plugins(self, key, files, data_key):
-        """Test all reader plugins, xfails for readers that don't have files.
-
-        Parameters
-        ----------
-        key : string
-           Reader plugin key
-        files : string
-           Wildcard path to data for reader
-        data_key : string
-           Key for data values in xarray dictionary
-
-        Returns
-        -------
-        None
-        """
-        file_path = self.data_dir + files
-        filelist = glob(file_path)
-        if files == "" or len(filelist) == 0:
-            pytest.xfail("No files given")
-
-        tmp_reader = readers.get_plugin(key)
-        inxr = tmp_reader(filelist[:2])
-        self.check_xr(inxr, data_key)
+    @pytest.mark.parametrize("reader_name", available_readers)
+    def test_reader_plugins(self, reader_name):
+        """Unit test every plugin, yeild xfail for ones with no unit tests."""
+        reader = readers.get_plugin(reader_name)
+        if not hasattr(reader.module, "yeild_test_files") or not hasattr(
+            reader.module, "yeild_test_parameters"
+        ):
+            pytest.xfail(reader_name + " has no test modules")
+        self.verify_plugin(reader)
