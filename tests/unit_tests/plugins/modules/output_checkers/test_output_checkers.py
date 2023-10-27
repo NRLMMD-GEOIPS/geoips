631b352b1c861e4065735730de419b2bf0539813
Jeremy Solbrig <1684491+jsolbrig@users.noreply.github.com>
2023-10-27 15:18:49 -0600

    Update test_output_checkers.py

--- tests/unit_tests/plugins/modules/output_checkers/test_output_checkers.py
+++ tests/unit_tests/plugins/modules/output_checkers/test_output_checkers.py
@@ -18,6 +18,11 @@ from geoips.commandline import log_setup
 
 log_setup.setup_logging()
 
+# If this is set to a path, this path will be used for storing the test data files
+# Otherwise a tmp_path directory will be used and deleted at the end of each test
+# For more information see here: https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html
+permanent_test_data_dir = None
+
 
 class TestOutputCheckers:
     """TestOutputChecker class, defining methods as well."""
@@ -28,17 +33,22 @@ class TestOutputCheckers:
         str(checker.name) for checker in output_checkers.get_plugins()
     ]
 
-    def compare_plugin(self, plugin):
+    def compare_plugin(self, tmp_path, plugin):
         """Test the comparision of two products with the appropriate Output Checker."""
-        compare_paths, output_paths = plugin.module.get_test_files()
+        if permanent_test_data_dir:
+            compare_paths, output_paths = plugin.module.get_test_files(
+                permanent_test_data_dir
+            )
+        else:
+            compare_paths, output_paths = plugin.module.get_test_files(tmp_path)
         plugin.module.perform_test_comparisons(plugin, compare_paths, output_paths)
 
     @pytest.mark.parametrize("checker_name", available_output_checkers)
-    def test_plugins(self, checker_name):
+    def test_plugins(self, tmp_path, checker_name):
         """Test all output_checkers that are ready for testing."""
         plugin = output_checkers.get_plugin(checker_name)
         if not hasattr(plugin.module, "get_test_files") or not hasattr(
             plugin.module, "perform_test_comparisons"
         ):
             pytest.xfail(checker_name + " is not ready to be tested yet.")
-        self.compare_plugin(plugin)
+        self.compare_plugin(tmp_path, plugin)
