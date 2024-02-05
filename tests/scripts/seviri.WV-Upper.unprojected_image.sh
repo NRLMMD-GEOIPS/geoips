ebbd04bc2790557bcac01e10d5b560fcc7bbfd42
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-02-05 15:03:17 -0500

    seviri procflow script updated with new test filenames; check beofre merging with main

--- tests/scripts/seviri.WV-Upper.unprojected_image.sh
+++ tests/scripts/seviri.WV-Upper.unprojected_image.sh
@@ -16,23 +16,33 @@
 
 # This exact test case required for valid comparisons - remove "compare_path" argument if running a different
 # set of arguments.
-run_procflow $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-_________-EPI______-202004040800-__ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-_________-PRO______-202004040800-__ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000001___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000002___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000003___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000004___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000005___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000006___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000007___-202004040800-C_ \
-             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000008___-202004040800-C_ \
+# run_procflow $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-_________-EPI______-202004040800-__ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-_________-PRO______-202004040800-__ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000001___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000002___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000003___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000004___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000005___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000006___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000007___-202004040800-C_ \
+#              $GEOIPS_TESTDATA_DIR/test_data_seviri/data/20200404.0800_meteoIO_tc2020sh24irondro/H-000-MSG1__-MSG1_IODC___-WV_062___-000008___-202004040800-C_ \
+run_procflow $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-_________-EPI______-202312110800-__ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-_________-PRO______-202312110800-__ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000001___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000002___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000003___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000004___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000005___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000006___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000007___-202312110800-C_ \
+             $GEOIPS_TESTDATA_DIR/test_data_seviri/data/H-000-MSG2__-MSG2_IODC___-WV_062___-000008___-202312110800-C_ \
              --procflow single_source \
              --reader_name seviri_hrit \
              --product_name WV-Upper \
              --output_formatter unprojected_image \
              --output_formatter_kwargs '{"x_size": "1000", "y_size": "1000"}' \
              --filename_formatter geoips_fname \
-             --compare_path "$GEOIPS_PACKAGES_DIR/geoips/tests/outputs/seviri.WV-Upper.unprojected_image" \
+             #--compare_path "$GEOIPS_PACKAGES_DIR/geoips/tests/outputs/seviri.WV-Upper.unprojected_image" \
              --self_register_dataset 'FULL_DISK' \
              --self_register_source seviri
 retval=$?
