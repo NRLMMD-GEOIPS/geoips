f24164329da644439c4c832fcd73a9dc024fc095
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2024-02-21 17:25:18 +0000

    Only raise errors if new attributes conflict with existing

--- geoips/commandline/log_setup.py
+++ geoips/commandline/log_setup.py
@@ -16,54 +16,97 @@ import logging
 import sys
 
 
-def add_logging_level(levelName, levelNum, methodName=None):
-    """
-    Comprehensively adds a new logging level to the `logging` module.
+class LogLevelAdder:
+    def __call__(self, levelName, levelNum, methodName=None):
+        """
+        Comprehensively adds a new logging level to the `logging` module.
 
-    Comprehensively adds a new logging level to the `logging` module
-    and the currently configured logging class.
+        Comprehensively adds a new logging level to the `logging` module
+        and the currently configured logging class.
 
-    `levelName` becomes an attribute of the `logging` module with the value
-    `levelNum`. `methodName` becomes a convenience method for both `logging`
-    itself and the class returned by `logging.getLoggerClass()` (usually just
-    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
-    used.
+        `levelName` becomes an attribute of the `logging` module with the value
+        `levelNum`. `methodName` becomes a convenience method for both `logging`
+        itself and the class returned by `logging.getLoggerClass()` (usually just
+        `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+        used.
 
-    To avoid accidental clobberings of existing attributes, this method will
-    raise an `AttributeError` if the level name is already an attribute of the
-    `logging` module or if the method name is already present
+        To avoid accidental clobberings of existing attributes, this method will
+        raise an `AttributeError` if the level name is already an attribute of the
+        `logging` module or if the method name is already present
 
-    Example
-    -------
-    >>> add_logging_level('TRACE', logging.DEBUG - 5)
-    >>> logging.getLogger(__name__).setLevel("TRACE")
-    >>> logging.getLogger(__name__).trace('that worked')
-    >>> logging.trace('so did this')
-    >>> logging.TRACE
-    5
+        Example
+        -------
+        >>> add_logging_level('TRACE', logging.DEBUG - 5)
+        >>> logging.getLogger(__name__).setLevel("TRACE")
+        >>> logging.getLogger(__name__).trace('that worked')
+        >>> logging.trace('so did this')
+        >>> logging.TRACE
+        5
 
-    """
-    if not methodName:
-        methodName = levelName.lower()
+        """
+        if not methodName:
+            methodName = levelName.lower()
 
-    if hasattr(logging, levelName):
-        raise AttributeError("{} already defined in logging module".format(levelName))
-    if hasattr(logging, methodName):
-        raise AttributeError("{} already defined in logging module".format(methodName))
-    if hasattr(logging.getLoggerClass(), methodName):
-        raise AttributeError("{} already defined in logger class".format(methodName))
+        # Add the log level
+        if hasattr(logging, levelName):
+            if getattr(logging, levelName) != levelNum:
+                raise AttributeError(
+                    f"{levelName} level already defined in logging module and its value "
+                    f"({getattr(logging, levelName)}) differs from the requested value "
+                    f"({levelNum})."
+                )
+        else:
+            logging.addLevelName(levelNum, levelName)
+            setattr(logging, levelName, levelNum)
 
-    def logForLevel(self, message, *args, **kwargs):
-        if self.isEnabledFor(levelNum):
-            self._log(levelNum, message, args, **kwargs)
+        logToRoot = self.get_logToRoot(levelNum)
+        if hasattr(logging, methodName):
+            if getattr(logging, methodName) != logToRoot:
+                raise AttributeError(
+                    f"{methodName} method already defined in logging module and differs "
+                    f"from the requested method."
+                )
+        else:
+            setattr(logging, methodName, logToRoot)
 
-    def logToRoot(message, *args, **kwargs):
-        logging.log(levelNum, message, *args, **kwargs)
+        loggerClass = logging.getLoggerClass()
+        logForLevel = self.get_logForLevel(levelNum)
+        if hasattr(loggerClass, methodName):
+            if getattr(loggerClass, methodName) != logForLevel:
+                raise AttributeError(
+                    f"{methodName} method already defined in logger class and differs "
+                    f"from the requested method."
+                )
+        else:
+            setattr(loggerClass, methodName, logForLevel)
 
-    logging.addLevelName(levelNum, levelName)
-    setattr(logging, levelName, levelNum)
-    setattr(logging.getLoggerClass(), methodName, logForLevel)
-    setattr(logging, methodName, logToRoot)
+    def get_logForLevel(self, levelNum):
+        if not hasattr(self, "_logForLevel_funcs"):
+            self._logForLevel_funcs = {}
+
+        if not levelNum in self._logForLevel_funcs:
+
+            def logForLevel(self, message, *args, **kwargs):
+                if self.isEnabledFor(levelNum):
+                    self._log(levelNum, message, args, **kwargs)
+
+            self._logForLevel_funcs[levelNum] = logForLevel
+        return self._logForLevel_funcs[levelNum]
+
+    def get_logToRoot(self, levelNum):
+        if not hasattr(self, "_logToRoot_funcs"):
+            self._logToRoot_funcs = {}
+
+        if not levelNum in self._logToRoot_funcs:
+
+            def logToRoot(message, *args, **kwargs):
+                logging.log(levelNum, message, *args, **kwargs)
+
+            self._logToRoot_funcs[levelNum] = logToRoot
+        return self._logToRoot_funcs[levelNum]
+
+
+add_logging_level = LogLevelAdder()
 
 
 def setup_logging(logging_level="INTERACTIVE", verbose=True):
