7b7977d82bffa838492a8e2e07316f78b575940e
evrose54 <mrewrose@gmail.com>
2024-03-06 20:55:10 +0000

    added comments throughout GeoipsValidate Class to improve readability of the code

--- geoips/commandline/geoips_validate.py
+++ geoips/commandline/geoips_validate.py
@@ -1,4 +1,4 @@
-"""GeoIPS CLI "get" command.
+"""GeoIPS CLI "validate" command.
 
 Retrieves the appropriate plugin/interface based on the arguments provided.
 """
@@ -43,6 +43,8 @@ class GeoipsValidate(GeoipsCommand):
             interface_type = "yaml_based"
             plugin = yaml.safe_load(open(fpath, "r"))
         try:
+            # if the module / yaml plugin is missing either interface or name, it's
+            # invalid and we need to report the error appropriately
             if interface_type == "module_based":
                 interface_name = plugin.interface
                 plugin_name = plugin.name
@@ -50,16 +52,20 @@ class GeoipsValidate(GeoipsCommand):
                 interface_name = plugin["interface"]
                 plugin_name = plugin["name"]
         except AttributeError or KeyError:
+            # Report such error.
             self.subcommand_parser.error(
                 err_str
             )
+        # get the correct interface and validate the plugin based on its name
         interface = getattr(interfaces, interface_name)
         is_valid = interface.plugin_is_valid(plugin_name)
         if not is_valid:
+            # if it's not valid, report that to the user
             self.subcommand_parser.error(
                 f"Plugin `{plugin_name}` found at {fpath} is invalid."
             )
         else:
+            # otherwise let them know they're good to go
             print(f"Plugin `{plugin_name}` found at {fpath} is valid.")
 
     def load_module_from_file(self, file_path, module_name=None):
