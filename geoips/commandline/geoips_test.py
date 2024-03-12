87606a07e332c7aca6c74d430376ccbec4e32564
evrose54 <mrewrose@gmail.com>
2024-03-12 19:29:42 +0000

    finished geoips test unit-test command

--- geoips/commandline/geoips_test.py
+++ geoips/commandline/geoips_test.py
@@ -5,6 +5,7 @@ Runs the appropriate tests based on the arguments provided.
 
 from glob import glob
 from importlib import resources
+from os import listdir
 from os.path import basename
 from subprocess import call
 
@@ -18,12 +19,68 @@ class GeoipsTestUnitTest(GeoipsExecutableCommand):
     subcommand_classes = []
 
     def add_arguments(self):
-        """Add arguments to the test-subparser for the Test Command."""
-        pass
+        """Instantiate the arguments that are supported for the test unit-test command.
+
+        Currently the "geoips test unit-test" command supports this format:
+            - geoips test unit-test <package_name> <script_name> -i
+        Where:
+            - <package_name> is any GeoIPS package that is installed and recognized by
+              the GeoIPS Library
+            - <script_name> is the name of the bash script being tested
+            - '-i' represents whether or not this script is an 'integration' test
+        """
+        self.subcommand_parser.add_argument(
+            "directory_name",
+            type=str,
+            help="GeoIPS Packages Unit Test Directory Name where unit tests are held.",
+        )
+        self.subcommand_parser.add_argument(
+            "--package_name",
+            "-p",
+            type=str,
+            default="geoips",
+            choices=self.plugin_packages,
+            help="GeoIPS Package containing the unit-tests to be ran.",
+        )
+        self.subcommand_parser.add_argument(
+            "--name_of_script",
+            "-n",
+            type=str,
+            default=None,
+            help="Specific Unit Test to be ran out of directory_name.",
+        )
 
     def __call__(self, args):
         """Run the provided unit tests based on the arguments provided."""
-        pass
+        dir_name = args.directory_name
+        package_name = args.package_name
+        script_name = args.name_of_script
+        unit_test_dir = str(resources.files(package_name) / f"../tests/unit_tests")
+        if dir_name not in listdir(unit_test_dir):
+            # The specified unit test directory does not exist at the specified location
+            # raise an error specifying that
+            err_str = f"Directory '{dir_name}' not found under {package_name}'s unit "
+            err_str += f"tests directory '{unit_test_dir}'. Please select one of the "
+            err_str += f"following unit tests:\n {listdir(unit_test_dir)}"
+            self.subcommand_parser.error(err_str)
+        elif script_name is not None:
+            # We've specified a specific Unit Test to run out of
+            # <package_name>/tests/unit_tests/<dir_name>/<script_name>, ensure that
+            # file actually exists
+            fnames = [
+                basename(fpath) for fpath in glob(f"{unit_test_dir}/{dir_name}/*.py")
+            ]
+            if script_name not in fnames:
+                err_str = f"Unit Test '{script_name}' not found under the directory "
+                err_str += f"'{unit_test_dir}', please select one of the options shown "
+                err_str += f"below.\ns {fnames}"
+                self.subcommand_parser.error(err_str)
+        else:
+            # script name wasn't specified, run all unit tests found under
+            # <package_name>/tests/unit_tests/<dir_name>
+            script_name = "."
+        test_path = str(f"{unit_test_dir}/{dir_name}/{script_name}")
+        call(f"pytest -v {test_path}", shell=True)
 
 
 class GeoipsTestScript(GeoipsExecutableCommand):
@@ -36,7 +93,7 @@ class GeoipsTestScript(GeoipsExecutableCommand):
         """Instantiate the arguments that are supported for the test script command.
 
         Currently the "geoips test script" command supports this format:
-            - geoips test script <package_name> <script_name> -i
+            - geoips test script -p <package_name> <script_name> -i
         Where:
             - <package_name> is any GeoIPS package that is installed and recognized by
               the GeoIPS Library
