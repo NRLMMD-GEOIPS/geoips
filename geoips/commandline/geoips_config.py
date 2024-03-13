45f3191c34e15ce45133c124da2e35c6c60cb38b
evrose54 <mrewrose@gmail.com>
2024-03-13 16:25:58 +0000

    refactored geoips config to implement downloading and extracting test data in a pythonic manner

--- geoips/commandline/geoips_config.py
+++ geoips/commandline/geoips_config.py
@@ -4,8 +4,9 @@ Various configuration-based commands for setting up your geoips environment.
 """
 
 from importlib import resources
-from os import listdir, environ
-from subprocess import call
+from os import listdir
+import requests
+import tarfile
 
 from geoips.commandline.geoips_command import GeoipsCommand, GeoipsExecutableCommand
 
@@ -40,7 +41,7 @@ class GeoipsConfigInstall(GeoipsExecutableCommand):
         test_dataset_name = args.test_dataset_name
         test_dataset_url = self.test_dataset_dict[test_dataset_name]
         script_dir = str(resources.files("geoips") / "../setup")
-        GEOIPS_TESTDATA_DIR = str(environ["GEOIPS_TESTDATA_DIR"])
+        GEOIPS_TESTDATA_DIR = str(resources.files("geoips") / "../../test_data")
         if test_dataset_name in listdir(GEOIPS_TESTDATA_DIR):
             out_str = f"Test dataset '{test_dataset_name}' already exists under "
             out_str += f"'{GEOIPS_TESTDATA_DIR}'. See that location for the contents "
@@ -52,12 +53,32 @@ class GeoipsConfigInstall(GeoipsExecutableCommand):
             print(
                 f"Installing {test_dataset_name} test dataset. This may take a while..."
             )
-            call(command, shell=True)
-            # output = call(command, shell=True)
+            self.download_extract_test_data(test_dataset_url, GEOIPS_TESTDATA_DIR)
             out_str = f"Test dataset '{test_dataset_name}' has been installed under "
             out_str += f"{GEOIPS_TESTDATA_DIR}/{test_dataset_name}/"
             print(out_str)
-            # return output
+
+    def download_extract_test_data(self, url, download_dir):
+        """Download the specified URL and write it to the corresponding download_dir.
+
+        Will extract the data using tarfile and create an archive by bundling the
+        associated files and directories together.
+
+        Parameters
+        ----------
+        url: str
+            - The url of the test dataset to download
+        download_dir: str
+            - The directory in which to download and extract the data into
+        """
+        resp = requests.get(url, stream=True, timeout=15)
+        if resp.status_code == 200:
+            with tarfile.open(fileobj=resp.raw, mode="r|gz") as tar:
+                tar.extractall(path=download_dir)
+        else:
+            self.subcommand_parser.error(
+                f"Error retrieving data from {url}; Status Code {resp.status_code}."
+            )
 
 
 class GeoipsConfig(GeoipsCommand):
