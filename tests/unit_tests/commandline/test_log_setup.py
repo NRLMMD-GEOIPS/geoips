4d260ed31460a6646b3c325bf969799baad66a0e
Evan Rose <62626811+evrose54@users.noreply.github.com>
2024-04-01 09:55:14 -0600

    377 add log with emphasis function (#401)
    * replaced log outputs with log_with_emphasis where applicable
    
    * replaced long ***... strings with multiplied * strings
    
    * added notes to release notes
    
    * black formatting changes
    
    * 1.12.0 release NRLMMD-GEOIPS/geoips#418
    
    * Removing unused comments
    
    * Update installation.rst
    
    * Update installation.rst
    
    * Update installation.rst
    
    * Add leading '/' to flake8 per-file-ignores
    
    Without the leading /, flake8 would fail from certain directories.
    Sometimes it would fail when running from within the repo, sometimes it would
    faile when running from OUTSIDE the repo.  With the leading /, it seems to
    pass from anywhere.
    
    * trying to get this branch back in order
    
    * new implementation of log_with_emphasis that sets a max length message per line
    
    * black formatting changes and static log_with_emphasis function implemented
    
    * update to black; new formatting changes applied
    
    * added unit test for log_with_emphasis
    
    * black formatting changes
    
    * updated release notes for this PR
    
    * flake8 changes for log_with_emphasis unit test
    
    * moved release notes for this branch to most current version v1_12_2a0.rst
    
    * remove test_log_interactive_non_geoips
    
    This test was problematic. It worked sometimes and not others.
    I decided it was better to just remove and not worry about polluting the logging class.
    Polluting the class doesn't actually hurt anything for anyone.
    
    * Move to correct file name
    
    * Fix docstring and imports
    
    * Add to changelog
    
    * Adding *** back in at beginning of message
    
    * Put it back, I think it broke black
    
    * updated log_with_emphasis function to accept one or more strings instead of a list of strings. Modified output to wrap messages differently
    
    * updated unit tests to reflect new changes made to log_with_emphasis function
    
    * made log_with_emphasis use python textwrap library and changed unit tests to reflect this
    
    * removed old emphasis log statements from output_checkers/netcdf.py
    
    * Update docstring for log_with_emphasis
    
    * Fix name of print_func
    
    * Ignore profiling stuff
    
    ---------
    
    Co-authored-by: mindyls <mindyls@users.noreply.github.com>
    Co-authored-by: mindyls <13873624+mindyls@users.noreply.github.com>
    Co-authored-by: Jeremy Solbrig <1684491+jsolbrig@users.noreply.github.com>
    Co-authored-by: Jeremy Solbrig <jeremy.solbrig@colostate.edu>

---
+++ tests/unit_tests/commandline/test_log_setup.py
@@ -0,0 +1,70 @@
+"""Unit tests for geoips/commandline/log_setup.py.
+
+Tests the following functions:
+- log_with_emphasis
+- add_log_level (by importing geoips.logging)
+"""
+
+import pytest
+
+import os
+import random
+import string
+from textwrap import wrap
+from geoips import logging
+from glob import glob
+from geoips.commandline.log_setup import log_with_emphasis
+
+LOG = logging.getLogger(__name__)
+
+
+def generate_random_string(length):
+    """Generate a random string of length :param length."""
+    return "".join(random.choices(string.ascii_letters, k=length))
+
+
+def generate_random_messages():
+    """Generate a random amount of messages with random length."""
+    num_messages = random.randint(1, 50)
+    return [generate_random_string(random.randint(5, 110)) for _ in range(num_messages)]
+
+
+@pytest.mark.parametrize("message", generate_random_messages())
+def test_log_with_emphasis(message, caplog):
+    """Pytest function for testing the output of 'log_with_emphasis'."""
+    caplog.set_level(logging.INFO)
+    max_message_len = min(74, len(message))
+    assert max_message_len <= 80, "Max emphasis in '*' is longer than 80 chars."
+    log_with_emphasis(LOG.info, message)
+    assert "*" * (max_message_len + 6) in caplog.text
+    for wmessage in wrap(message, width=74):
+        assert "** " + wmessage in caplog.text
+    assert "*" * (max_message_len + 6) in caplog.text
+    assert "\n" in caplog.text
+
+
+def test_log_interactive_geoips(caplog):
+    """Test log.interactive using logging from geoips."""
+    log = logging.getLogger(__name__)
+
+    log.interactive("FROM PYTEST")
+    assert "FROM PYTEST" in caplog.text
+    assert "INTERACTIVE" in caplog.text
+
+
+def test_log_interactive_from_directly_imported_plugin(caplog):
+    """Use ABI reader, which calls log.interactive.
+
+    It used to be that plugins loaded via get_plugin outside of GeoIPS could not call
+    log.interactive. They would raise an AttributeError. This is to ensure that problem
+    has been fixed and remains fixed.
+    """
+    from geoips.interfaces import readers
+
+    reader = readers.get_plugin("abi_netcdf")
+
+    test_data_dir = os.getenv("GEOIPS_TESTDATA_DIR")
+    test_data_dir += "/test_data_noaa_aws/data/goes16/20200918/1950"
+    test_data_files = glob(test_data_dir + "/*C14*.nc")
+    _ = reader(test_data_files)
+    assert "INTERACTIVE" in caplog.text
