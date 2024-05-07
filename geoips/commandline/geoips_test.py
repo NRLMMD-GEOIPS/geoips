eb4e2058e5dbd0545eaf11f0ec3e4583e90eb577
evrose54 <mrewrose@gmail.com>
2024-05-07 17:46:49 +0000

    commented out 'geoips test unit-test' code for the time being

--- geoips/commandline/geoips_test.py
+++ geoips/commandline/geoips_test.py
@@ -5,96 +5,96 @@ Runs the appropriate tests based on the arguments provided.
 
 from glob import glob
 from importlib import resources
-from os import listdir
+# from os import listdir
 from os.path import basename
-from pytest import main as invoke_pytest
+# from pytest import main as invoke_pytest
 from subprocess import call
 
 from geoips.commandline.geoips_command import GeoipsCommand, GeoipsExecutableCommand
 
 
-class GeoipsTestUnitTest(GeoipsExecutableCommand):
-    """Test Sub-Command for running GeoIPS Unit Tests."""
+# class GeoipsTestUnitTest(GeoipsExecutableCommand):
+#     """Test Sub-Command for running GeoIPS Unit Tests."""
 
-    subcommand_name = "unit-test"
-    subcommand_classes = []
+#     subcommand_name = "unit-test"
+#     subcommand_classes = []
 
-    def add_arguments(self):
-        """Instantiate the arguments that are supported for the test unit-test command.
+#     def add_arguments(self):
+#         """Instantiate the arguments that are supported for the test unit-test command.
 
-        Currently the "geoips test unit-test" command supports this format:
-            - geoips test unit-test dir_name <-p> <package_name> <-n> <test_name>
-        Where:
-            - dir_name is the name of the folder containing the unit-test[s] you want to
-              run
-            - <package_name> is any GeoIPS package that is installed and recognized by
-              the GeoIPS Library
-            - <test_name> is the name of the unit test being ran
-        """
-        self.subcommand_parser.add_argument(
-            "directory_name",
-            type=str,
-            help="GeoIPS Packages Unit Test Directory Name where unit tests are held.",
-        )
-        self.subcommand_parser.add_argument(
-            "--package_name",
-            "-p",
-            type=str,
-            default="geoips",
-            choices=self.plugin_packages,
-            help="GeoIPS Package containing the unit-tests to be ran.",
-        )
-        self.subcommand_parser.add_argument(
-            "--name_of_test",
-            "-n",
-            type=str,
-            default=None,
-            help="Specific Unit Test to be ran out of directory_name.",
-        )
+#         Currently the "geoips test unit-test" command supports this format:
+#             - geoips test unit-test dir_name <-p> <package_name> <-n> <test_name>
+#         Where:
+#             - dir_name is the name of the folder containing the unit-test[s] you want to
+#               run
+#             - <package_name> is any GeoIPS package that is installed and recognized by
+#               the GeoIPS Library
+#             - <test_name> is the name of the unit test being ran
+#         """
+#         self.subcommand_parser.add_argument(
+#             "directory_name",
+#             type=str,
+#             help="GeoIPS Packages Unit Test Directory Name where unit tests are held.",
+#         )
+#         self.subcommand_parser.add_argument(
+#             "--package_name",
+#             "-p",
+#             type=str,
+#             default="geoips",
+#             choices=self.plugin_packages,
+#             help="GeoIPS Package containing the unit-tests to be ran.",
+#         )
+#         self.subcommand_parser.add_argument(
+#             "--name_of_test",
+#             "-n",
+#             type=str,
+#             default=None,
+#             help="Specific Unit Test to be ran out of directory_name.",
+#         )
 
-    def __call__(self, args):
-        """Run the provided unit tests based on the arguments provided."""
-        dir_name = args.directory_name
-        package_name = args.package_name
-        test_name = args.name_of_test
-        unit_test_dir = str(resources.files(package_name) / "../tests/unit_tests")
+#     def __call__(self, args):
+#         """Run the provided unit tests based on the arguments provided."""
+#         dir_name = args.directory_name
+#         package_name = args.package_name
+#         test_name = args.name_of_test
+#         unit_test_dir = str(resources.files(package_name) / "../tests/unit_tests")
 
-        try:
-            # Try listing the expected unit test directory. If it fails, raise an
-            # argparse error which states such package doesn't have a unit tests
-            # directory
-            listdir(unit_test_dir)
-        except FileNotFoundError:
-            err_str = f"No unit tests directory found for package '{package_name}'."
-            self.subcommand_parser.error(err_str)
+#         try:
+#             # Try listing the expected unit test directory. If it fails, raise an
+#             # argparse error which states such package doesn't have a unit tests
+#             # directory
+#             listdir(unit_test_dir)
+#         except FileNotFoundError:
+#             err_str = f"No unit tests directory found for package '{package_name}'."
+#             self.subcommand_parser.error(err_str)
 
-        if dir_name not in listdir(unit_test_dir):
-            # The specified unit test directory does not exist at the specified location
-            # raise an error specifying that
-            err_str = f"Directory '{dir_name}' not found under {package_name}'s unit "
-            err_str += f"tests directory '{unit_test_dir}'. Please select one of the "
-            err_str += f"following unit test directories:\n {listdir(unit_test_dir)}"
-            self.subcommand_parser.error(err_str)
-        elif test_name is not None:
-            # We've specified a specific Unit Test to run out of
-            # <package_name>/tests/unit_tests/<dir_name>/<script_name>, ensure that
-            # file actually exists
-            fnames = [
-                basename(fpath)
-                for fpath in glob(f"{unit_test_dir}/{dir_name}/test_*.py")
-            ]
-            if test_name not in fnames:
-                err_str = f"Unit Test '{test_name}' not found under the directory "
-                err_str += f"'{unit_test_dir}', please select one of the options shown "
-                err_str += f"below.\ns {fnames}"
-                self.subcommand_parser.error(err_str)
-        else:
-            # script name wasn't specified, run all unit tests found under
-            # <package_name>/tests/unit_tests/<dir_name>
-            test_name = "."
+#         if dir_name not in listdir(unit_test_dir):
+#             # The specified unit test directory does not exist at the specified location
+#             # raise an error specifying that
+#             err_str = f"Directory '{dir_name}' not found under {package_name}'s unit "
+#             err_str += f"tests directory '{unit_test_dir}'. Please select one of the "
+#             err_str += f"following unit test directories:\n {listdir(unit_test_dir)}"
+#             self.subcommand_parser.error(err_str)
+#         elif test_name is not None:
+#             # We've specified a specific Unit Test to run out of
+#             # <package_name>/tests/unit_tests/<dir_name>/<script_name>, ensure that
+#             # file actually exists
+#             fnames = [
+#                 basename(fpath)
+#                 for fpath in glob(f"{unit_test_dir}/{dir_name}/test_*.py")
+#             ]
+#             if test_name not in fnames:
+#                 err_str = f"Unit Test '{test_name}' not found under the directory "
+#                 err_str += f"'{unit_test_dir}', please select one of the options shown "
+#                 err_str += f"below.\ns {fnames}"
+#                 self.subcommand_parser.error(err_str)
+#         else:
+#             # script name wasn't specified, run all unit tests found under
+#             # <package_name>/tests/unit_tests/<dir_name>
+#             test_name = "."
 
-        test_path = str(f"{unit_test_dir}/{dir_name}/{test_name}")
-        invoke_pytest(["-v", test_path])
+#         test_path = str(f"{unit_test_dir}/{dir_name}/{test_name}")
+#         invoke_pytest(["-v", test_path])
 
 
 class GeoipsTestScript(GeoipsExecutableCommand):
@@ -209,4 +209,5 @@ class GeoipsTest(GeoipsCommand):
     """Top-Level Test Command for testing GeoIPS and its corresponding Packages."""
 
     subcommand_name = "test"
-    subcommand_classes = [GeoipsTestLinting, GeoipsTestScript, GeoipsTestUnitTest]
+    subcommand_classes = [GeoipsTestLinting, GeoipsTestScript]
+    # subcommand_classes = [GeoipsTestLinting, GeoipsTestScript, GeoipsTestUnitTest]
--- tests/unit_tests/commandline/test_geoips_test_unit_test.py
+++ tests/unit_tests/commandline/test_geoips_test_unit_test.py
@@ -3,108 +3,112 @@
 See geoips/commandline/ancillary_info/cmd_instructions.yaml for more information.
 """
 
-from glob import glob
-from numpy.random import rand
-from os import listdir
-from os.path import basename
-import pytest
-from importlib.resources import files
+# from glob import glob
+# from numpy.random import rand
+# from os import listdir
+# from os.path import basename
+# import pytest
+# from importlib.resources import files
 
-from tests.unit_tests.commandline.cli_top_level_tester import BaseCliTest
-
-
-class TestGeoipsTestUnitTest(BaseCliTest):
-    """Unit Testing Class for Test Unit Test Sub-Command."""
-
-    @property
-    def all_possible_subcommand_combinations(self):
-        """A stochastic list of commands used by the GeoipsTestUnitTest command.
-
-        This includes failing cases as well.
-        """
-        if not hasattr(self, "_cmd_list"):
-            base_args = self._test_unit_test_args
-            self._cmd_list = [
-                base_args + ["commandline", "-n", "test_geoips_list_packages.py"]
-            ]
-            # select a small random amount of tests to call via geoips test unit-test
-            for pkg_name in self.plugin_packages:
-                unit_test_dir = str(files(pkg_name) / "../tests/unit_tests")
-                try:
-                    listdir(unit_test_dir)
-                except FileNotFoundError:
-                    continue
-                for dir_name in listdir(unit_test_dir):
-                    for test_name in glob(f"{unit_test_dir}/{dir_name}/test*.py"):
-                        test_name = basename(test_name)
-                        do_geoips_test = rand() < 0.05
-                        if do_geoips_test:
-                            self._cmd_list.append(
-                                base_args + ["-p", pkg_name, dir_name, "-n", test_name]
-                            )
-            # Add argument list to retrieve help message
-            self._cmd_list.append(base_args + ["-h"])
-            # Add argument list with non existent package and directory name
-            self._cmd_list.append(
-                base_args + ["-p", "non_existent_package", "non_existent_dirname"]
-            )
-        return self._cmd_list
-
-    def check_error(self, args, error):
-        """Ensure that the 'geoips test unit-test ...' error output is correct.
-
-        Parameters
-        ----------
-        args: 2D list of str
-            - The arguments used to call the CLI (expected to fail)
-        error: str
-            - Multiline str representing the error output of the CLI call
-        """
-        # An error occurred using args. Assert that args is not valid and check the
-        # output of the error.
-        err_str = "To use, type\n`geoips test unit-test -p <package_name> "
-        err_str += "<directory_name> <-n> <script_name>`"
-        assert err_str in error
-
-    def check_output(self, args, output):
-        """Ensure that the 'geoips unit-test ...' successful output is correct.
-
-        Parameters
-        ----------
-        args: 2D list of str
-            - The arguments used to call the CLI
-        output: str
-            - Multiline str representing the output of the CLI call
-        """
-        # The args provided are valid, so test that the output is actually correct
-        if "-h" in args:
-            help_str = "To use, type\n`geoips test unit-test -p <package_name> "
-            help_str += "<directory_name> <-n> <script_name>`"
-            assert help_str in output
-        else:
-            # Checking that output from geoips unit-test command reports succeeds
-            assert "test session starts" in output
-            assert "pytest" in output
+# from tests.unit_tests.commandline.cli_top_level_tester import BaseCliTest
 
 
-test_sub_cmd = TestGeoipsTestUnitTest()
+# class TestGeoipsTestUnitTest(BaseCliTest):
+#     """Unit Testing Class for Test Unit Test Sub-Command."""
+
+#     @property
+#     def all_possible_subcommand_combinations(self):
+#         """A stochastic list of commands used by the GeoipsTestUnitTest command.
+
+#         This includes failing cases as well.
+#         """
+#         if not hasattr(self, "_cmd_list"):
+#             base_args = self._test_unit_test_args
+#             self._cmd_list = [
+#                 base_args + ["commandline", "-n", "test_geoips_list_packages.py"]
+#             ]
+#             # select a small random amount of tests to call via geoips test unit-test
+#             for pkg_name in self.plugin_packages:
+#                 unit_test_dir = str(files(pkg_name) / "../tests/unit_tests")
+#                 try:
+#                     listdir(unit_test_dir)
+#                 except FileNotFoundError:
+#                     continue
+#                 for dir_name in listdir(unit_test_dir):
+#                     for test_name in glob(f"{unit_test_dir}/{dir_name}/test*.py"):
+#                         test_name = basename(test_name)
+#                         do_geoips_test = rand() < 0.05
+#                         if do_geoips_test:
+#                             self._cmd_list.append(
+#                                 base_args + ["-p", pkg_name, dir_name, "-n", test_name]
+#                             )
+#             # Add argument list to retrieve help message
+#             self._cmd_list.append(base_args + ["-h"])
+#             # Add argument list with non existent package and directory name
+#             self._cmd_list.append(
+#                 base_args + ["-p", "non_existent_package", "non_existent_dirname"]
+#             )
+#         return self._cmd_list
+
+#     def check_error(self, args, error):
+#         """Ensure that the 'geoips test unit-test ...' error output is correct.
+
+#         Parameters
+#         ----------
+#         args: 2D list of str
+#             - The arguments used to call the CLI (expected to fail)
+#         error: str
+#             - Multiline str representing the error output of the CLI call
+#         """
+#         # An error occurred using args. Assert that args is not valid and check the
+#         # output of the error.
+#         err_str = "To use, type\n`geoips test unit-test -p <package_name> "
+#         err_str += "<directory_name> <-n> <script_name>`"
+#         assert err_str in error
+
+#     def check_output(self, args, output):
+#         """Ensure that the 'geoips unit-test ...' successful output is correct.
+
+#         Parameters
+#         ----------
+#         args: 2D list of str
+#             - The arguments used to call the CLI
+#         output: str
+#             - Multiline str representing the output of the CLI call
+#         """
+#         # The args provided are valid, so test that the output is actually correct
+#         if "-h" in args:
+#             help_str = "To use, type\n`geoips test unit-test -p <package_name> "
+#             help_str += "<directory_name> <-n> <script_name>`"
+#             assert help_str in output
+#         else:
+#             # Checking that output from geoips unit-test command reports succeeds
+#             assert "test session starts" in output
+#             assert "pytest" in output
 
 
-@pytest.mark.parametrize(
-    "args",
-    test_sub_cmd.all_possible_subcommand_combinations,
-    ids=test_sub_cmd.generate_id,
-)
-def test_all_command_combinations(args):
-    """Test all 'geoips test unit-test ...' commands.
+# test_sub_cmd = TestGeoipsTestUnitTest()
 
-    This test covers every valid combination of commands for the 'geoips test unit-test'
-    command. We also test invalid commands, to ensure that the proper help documentation
-    is provided for those using the command incorrectly.
 
-    Parameters
-    ----------
-    args: 2D array of str
-        - List of arguments to call the CLI with (ie. ['geoips', 'test', 'unit-test'])
-    """
-    test_sub_cmd.test_all_command_combinations(args)
+# @pytest.mark.parametrize(
+#     "args",
+#     test_sub_cmd.all_possible_subcommand_combinations,
+#     ids=test_sub_cmd.generate_id,
+# )
+# def test_all_command_combinations(args):
+#     """Test all 'geoips test unit-test ...' commands.
+
+#     This test covers every valid combination of commands for the 'geoips test unit-test'
+#     command. We also test invalid commands, to ensure that the proper help documentation
+#     is provided for those using the command incorrectly.
+
+#     Parameters
+#     ----------
+#     args: 2D array of str
+#         - List of arguments to call the CLI with (ie. ['geoips', 'test', 'unit-test'])
+#     """
+#     test_sub_cmd.test_all_command_combinations(args)
+
+# THIS SECTION WAS COMMENTED OUT BECAUSE WE DON"T WANT TO INVOKE PYTEST IN MAIN GEOIPS
+# AND THIS COMMAND IS LARGELY FOR EASE OF USE'S SAKE. WE MAY WANT TO USE IT LATER SO
+# THAT IS WHY IT HAS NOT BEEN DELETED ALTOGETHER.
