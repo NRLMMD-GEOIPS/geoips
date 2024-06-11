5ae2e73a805aef8e2153627f5ff5368fd5d11f4d
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-06-11 15:05:47 -0600

    Updates - will likely remove this branch create new repo

--- docs/build_release_notes.py
+++ docs/build_release_notes.py
@@ -10,9 +10,7 @@ from datetime import datetime
 
 import pygit2
 from rich.console import Console
-
-console = Console()
-
+from rich_argparse import RichHelpFormatter
 
 default_categories = [
     "bug-fix",
@@ -31,6 +29,13 @@ default_title = "NO TITLE"
 default_description = "NO DESCRIPTION"
 
 
+def get_rich_opener(no_format=False):
+    if no_format:
+        return rich.progress.Progress().open
+    else:
+        return rich.progress.open
+
+
 def parse_arguments():
     """
     Parse command line arguments for input folder and output file.
@@ -41,7 +46,8 @@ def parse_arguments():
         Parsed arguments containing input_folder and output_file.
     """
     parser = argparse.ArgumentParser(
-        description="Generate release notes from YAML files."
+        description="Generate release notes from YAML files.",
+        formatter_class=RichHelpFormatter,
     )
     parser.add_argument(
         "-t",
@@ -68,20 +74,44 @@ def parse_arguments():
         help="Version number of the release. Default is '[UNKNOWN]'.",
         required=False,
     )
-
     parser.add_argument(
-        "-head",
-        "--header_file",
+        "-r",
+        "--release-date",
+        type=str,
+        help="Date of the release. Default is current system time.",
+        required=False,
+    )
+    parser.add_argument(
+        "-nc",
+        "--no-color",
+        action="store_true",
+        default=False,
+        help="Disable text formatting for CLI output.",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix-file",
         type=str,
         help="A header file to prepend to the release notes.",
     )
     parser.add_argument(
-        "-o", "--output_file", type=str, help="The output file for release notes."
+        "-s",
+        "--suffix-file",
+        type=str,
+        help="A footer file to suffix to the release notes.",
     )
+    parser.add_argument(
+        "-o", "--output-file", type=str, help="The output file for release notes."
+    )
+    parser.add_argument(
+        "-nr", "--no-rich", action="store_true", help="Disable rich text output"
+    )
+    parser.add_argument("-q", "--quiet", action="store_true", help="Only output errors")
+
     return parser.parse_args(), parser
 
 
-def exit_on_invalid_arguments(args, parser):
+def exit_on_invalid_arguments(args, parser, console):
     """
     Validate the argparse arguments.
 
@@ -114,12 +144,12 @@ def get_yaml_template_path(file_path_arg):
         filepath = os.getcwd()
         filename = f"{get_current_git_branch()}.yaml"
         return os.path.join(filepath, filename)
-    if not file_path_arg.endswith(".yaml"):
+    if not (file_path_arg.endswith(".yaml") or file_path_arg.endswith(".yml")):
         return file_path_arg + ".yaml"
     return file_path_arg
 
 
-def create_blank_template_yaml_file(file_path_arg):
+def create_blank_template_yaml_file(file_path_arg, console):
     """
     Create a blank YAML file with default categories.
 
@@ -131,7 +161,15 @@ def create_blank_template_yaml_file(file_path_arg):
     default_yaml = {
         category: [{"title": "", "description": ""}] for category in default_categories
     }
-    yaml_template_path = get_yaml_template_path(file_path_arg)
+    try:
+        yaml_template_path = get_yaml_template_path(file_path_arg)
+    except pygit2.GitError:
+        console.print(
+            "[bold red]Could not find a git repo. Please run in a "
+            + "git repo or pass a file path for the yaml template "
+            + "(eg '-t /path/to/file.yaml')."
+        )
+        exit(1)
     with open(yaml_template_path, "w") as file:
         yaml.dump(default_yaml, file)
 
@@ -188,7 +226,7 @@ def exit_on_invalid_yaml(content, file_path):
                 )
 
 
-def read_yaml_files(input_files):
+def read_yaml_files(input_files, rich_open):
     """
     Read and parse the given list of YAML files.
 
@@ -215,9 +253,7 @@ def read_yaml_files(input_files):
     """
     data = {}
     for file_path in input_files:
-        with rich.progress.open(
-            file_path, "r", description=f"Reading {file_path}"
-        ) as file:
+        with rich_open(file_path, "r", description=f"Reading {file_path}") as file:
             content = yaml.safe_load(file)
             exit_on_invalid_yaml(content, file_path)
             for category, entries in content.items():
@@ -299,21 +335,20 @@ def get_yaml_files_from_input(input_files_or_folders):
     yaml_files = []
     for path in input_files_or_folders:
         if os.path.isfile(path):
-            if not path.endswith(".yaml"):
+            if not path.endswith(".yaml") or path.endswith(".yml"):
                 raise ValueError(f"File {path} is not a YAML file.")
             yaml_files.append(path)
         elif os.path.isdir(path):
             dir_yaml_files = []
             for root, dirs, files in os.walk(path):
                 for file in files:
-                    if file.endswith(".yaml"):
+                    if file.endswith(".yaml") or file.endswith(".yml"):
                         dir_yaml_files.append(os.path.join(root, file))
             if len(dir_yaml_files) == 0:
                 raise ValueError(f"No YAML files found in directory {path}.")
             yaml_files.extend(dir_yaml_files)
         else:
-            console.print(f"[red]Invalid file or directory: [bold]{path}[/]")
-            exit(1)
+            raise FileNotFoundError(path)
     return yaml_files
 
 
@@ -330,7 +365,6 @@ def write_output_file(output_file, content):
     """
     with open(output_file, "w") as file:
         file.write(content)
-        console.print(f"[green]Wrote release notes to {output_file}")
 
 
 def get_current_git_branch():
@@ -342,16 +376,72 @@ def get_current_git_branch():
     str
         The name of the current git branch.
     """
-    try:
-        repo = pygit2.Repository(".")
-    except pygit2.GitError:
-        console.print(
-            "[bold red]Could not find a git repo. Please run in a "
-            + "git repo or pass a file path for the yaml template "
-            + "(eg '-t /path/to/file.yaml')."
-        )
-        exit(1)
+    repo = pygit2.Repository(".")
     return repo.head.shorthand
+
+
+def add_header_footer(content, rich_open, header_file=None, footer_file=None):
+    def getFile(file):
+        with rich_open(file, "r", description=f"Reading {file}") as file:
+            return file.read()
+
+    if header_file:
+        content = getFile(header_file) + "\n" + content
+    if footer_file:
+        content = content + "\n" + getFile(header_file)
+    return content
+
+
+def build_release_notes(
+    input_files_or_folders,
+    output_file,
+    console,
+    rich_open,
+    version=None,
+    release_date=None,
+    header_file=None,
+    footer_file=None,
+):
+    """
+    Build release notes from YAML data.
+
+    Parameters
+    ----------
+    data : dict
+        Parsed content of YAML files.
+    version : str, optional
+        Version number of the release, by default '1.1'.
+    release_date : str, optional
+        Release date, by default None, which uses today's date.
+    header_file : str, optional
+        A header file to prepend to the release notes.
+    footer_file : str, optional
+        A footer file to suffix to the release notes.
+
+    Returns
+    -------
+    str
+        Formatted release notes in .rst format.
+    """
+    try:
+        yaml_files = get_yaml_files_from_input(input_files_or_folders)
+    except FileNotFoundError as e:
+        console.print(f"[red]Invalid file or directory: [bold]{e}[/]")
+        exit(1)
+    except ValueError as e:
+        console.print(f"[red]{e}")
+        exit(1)
+    data = read_yaml_files(yaml_files, rich_open)
+    content = format_release_notes(data, version=version)
+    content = add_header_footer(content, header_file, footer_file)
+    write_output_file(output_file, content)
+    console.print(f"[green]Wrote release notes to {output_file}")
+    return content
+
+
+def setup_console(no_format=False, quiet=False):
+    console = Console(quiet=quiet, no_color=no_format)
+    return console
 
 
 def main():
@@ -359,20 +449,24 @@ def main():
     Main function to generate release notes from YAML files and write to an output file.
     """
     args, parser = parse_arguments()
-    exit_on_invalid_arguments(args, parser)
+
+    console = setup_console(args.no_rich, args.quiet)
+    rich_open = get_rich_opener(args.no_rich)
+
+    exit_on_invalid_arguments(args, parser, console)
     if "write_yaml_template" in args:
-        create_blank_template_yaml_file(args.write_yaml_template)
+        create_blank_template_yaml_file(args.write_yaml_template, console)
     if args.input_files_or_folders:
-        yaml_files = get_yaml_files_from_input(args.input_files_or_folders)
-        data = read_yaml_files(yaml_files)
-        content = format_release_notes(data, version=args.version)
-        if args.header_file:
-            with rich.progress.open(
-                args.header_file, "r", description=f"Reading {args.header_file}"
-            ) as file:
-                header = file.read()
-                content = header + "\n" + content
-        write_output_file(args.output_file, content)
+        build_release_notes(
+            args.input_files_or_folders,
+            args.output_file,
+            console,
+            rich_open,
+            version=args.version,
+            release_date=args.release_date,
+            header_file=args.prefix_file,
+            footer_file=args.suffix_file,
+        )
 
 
 if __name__ == "__main__":
