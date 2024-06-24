7ae1c1648d13cf9c7f600b41df321631cd3d0948
evrose54 <mrewrose@gmail.com>
2024-06-24 19:24:50 +0000

    refactored get package unit test to use alias mapping

--- tests/unit_tests/commandline/test_geoips_get_package.py
+++ tests/unit_tests/commandline/test_geoips_get_package.py
@@ -22,8 +22,8 @@ class TestGeoipsGetPackage(BaseCliTest):
             base_args = ["geoips", "get"]
             # add arguments for retrieving each package
             for pkg_name in self.plugin_package_names:
-                self._cmd_list.append(base_args + ["pkg", pkg_name])
-                self._cmd_list.append(base_args + ["package", pkg_name])
+                for alias in self.alias_mapping["package"] + ["package"]:
+                    self._cmd_list.append(base_args + [alias, pkg_name])
             # Add argument list to retrieve help message
             self._cmd_list.append(base_args + ["pkg", "-h"])
             self._cmd_list.append(base_args + ["package", "-h"])
