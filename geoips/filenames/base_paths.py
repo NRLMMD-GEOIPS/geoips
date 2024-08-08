b239f6a5356e39cb23a376db22d8ae618710d633
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-08-08 10:29:30 -0600

    Refactor path setting

--- geoips/filenames/base_paths.py
+++ geoips/filenames/base_paths.py
@@ -21,12 +21,12 @@ LOG = logging.getLogger(__name__)
 if not os.getenv("GEOIPS_OUTDIRS"):
     raise KeyError(
         "GEOIPS_OUTDIRS must be set in your environment."
-        "Please set GEOIPS_OUTDIRS and try again"
+        "Please set GEOIPS_OUTDIRS and try again."
     )
 
 
-def get_env_path(var_name, default_path, rstrip_path=True):
-    """Retrieve environment variable or return a default path."""
+def get_env_var(var_name, default, rstrip_path=True):
+    """Retrieve environment variable or provided default, optionally rstrip a '/'."""
     env_value = os.getenv(var_name)
     if env_value:
         if rstrip_path:
@@ -34,7 +34,7 @@ def get_env_path(var_name, default_path, rstrip_path=True):
         else:
             return env_value
     else:
-        return default_path
+        return default
 
 
 def initialize_paths():
@@ -43,130 +43,94 @@ def initialize_paths():
 
     # Base and Documentation Paths
     paths["BASE_PATH"] = os.path.join(os.path.dirname(__file__), "..")
-    paths["GEOIPS_DOCS_URL"] = r"https://nrlmmd-geoips.github.io/geoips/"
-
-    # Operational User
-    paths["GEOIPS_OPERATIONAL_USER"] = os.getenv("GEOIPS_OPERATIONAL_USER", False)
 
     # Output Directories
-    geoips_outdirs = os.getenv("GEOIPS_OUTDIRS")
-    if not geoips_outdirs:
+    if not os.getenv("GEOIPS_OUTDIRS"):
         raise KeyError(
             "GEOIPS_OUTDIRS must be set in your environment. "
             "Please set GEOIPS_OUTDIRS and try again"
         )
-    paths["GEOIPS_OUTDIRS"] = geoips_outdirs.rstrip("/")
-
-    # Package Directories
-    geoips_packages_dir = get_env_path(
-        "GEOIPS_PACKAGES_DIR", os.path.join(paths["BASE_PATH"], "..", "..")
-    )
-    if os.path.exists(geoips_packages_dir):
-        paths["GEOIPS_PACKAGES_DIR"] = geoips_packages_dir
-
-    # Base Directory
-    paths["GEOIPS_BASEDIR"] = get_env_path(
+    paths["GEOIPS_OUTDIRS"] = os.getenv("GEOIPS_OUTDIRS").rstrip("/")
+    paths["GEOIPS_PACKAGES_DIR"] = (os.path.join(paths["BASE_PATH"], "..", ".."),)
+    paths["GEOIPS_BASEDIR"] = get_env_var(
         "GEOIPS_BASEDIR", os.path.join(paths["GEOIPS_PACKAGES_DIR"], "..")
     )
 
-    # Test Data Directory
-    paths["GEOIPS_TESTDATA_DIR"] = get_env_path(
-        "GEOIPS_TESTDATA_DIR", paths["GEOIPS_BASEDIR"] + "/test_data"
-    )
-
-    # Dependencies Directory
-    paths["GEOIPS_DEPENDENCIES_DIR"] = get_env_path(
-        "GEOIPS_DEPENDENCIES_DIR", paths["GEOIPS_BASEDIR"] + "/geoips_dependencies"
-    )
-
-    # Data Processing Paths
-    data_paths = {
-        "PRESECTORED_DATA_PATH": "preprocessed/sectored",
-        "PREREAD_DATA_PATH": "preprocessed/unsectored",
-        "PREREGISTERED_DATA_PATH": "preprocessed/registered",
-        "PRECALCULATED_DATA_PATH": "preprocessed/algorithms",
-        "CLEAN_IMAGERY_PATH": "preprocessed/clean_imagery",
-        "ANNOTATED_IMAGERY_PATH": "preprocessed/annotated_imagery",
-        "FINAL_DATA_PATH": "preprocessed/final",
-        "PREGENERATED_GEOLOCATION_PATH": "preprocessed/geolocation",
+    geoips_global_variables = {
+        # GeoIPS Documentation URL
+        "GEOIPS_DOCS_URL": r"https://nrlmmd-geoips.github.io/geoips/",
+        # Version
+        "GEOIPS_VERS": os.getenv("GEOIPS_VERS", "0.0.0"),
+        # Operational User
+        "GEOIPS_OPERATIONAL_USER": os.getenv("GEOIPS_OPERATIONAL_USER", False),
+        # Copyright Information
+        "GEOIPS_COPYRIGHT": os.getenv("GEOIPS_COPYRIGHT", "NRL-Monterey"),
+        "GEOIPS_COPYRIGHT_ABBREVIATED": os.getenv(
+            "GEOIPS_COPYRIGHT_ABBREVIATED", "NRLMRY"
+        ),
+        # Configuration and Queue
+        "GEOIPS_RCFILE": os.getenv("GEOIPS_RCFILE", ""),
+        "DEFAULT_QUEUE": os.getenv("DEFAULT_QUEUE", None),
+        # Computer Identifier
+        "BOXNAME": socket.gethostname(),
     }
 
-    for key, subdir in data_paths.items():
-        paths[key] = get_env_path(key, os.path.join(paths["GEOIPS_OUTDIRS"], subdir))
+    paths.update(geoips_global_variables)
 
-    # Copyright Information
-    paths["GEOIPS_COPYRIGHT"] = os.getenv("GEOIPS_COPYRIGHT", "NRL-Monterey")
-    paths["GEOIPS_COPYRIGHT_ABBREVIATED"] = os.getenv(
-        "GEOIPS_COPYRIGHT_ABBREVIATED", "NRLMRY"
-    )
+    derivative_directory_paths = {
+        paths["GEOIPS_BASEDIR"]: {
+            "GEOIPS_TESTDATA_DIR": "test_data",
+            "GEOIPS_DEPENDENCIES_DIR": "geoips_dependencies",
+        },
+        paths["GEOIPS_OUTDIRS"]: {
+            # Data Output Paths
+            "PRESECTORED_DATA_PATH": "preprocessed/sectored",
+            "PREREAD_DATA_PATH": "preprocessed/unsectored",
+            "PREREGISTERED_DATA_PATH": "preprocessed/registered",
+            "PRECALCULATED_DATA_PATH": "preprocessed/algorithms",
+            "CLEAN_IMAGERY_PATH": "preprocessed/clean_imagery",
+            "ANNOTATED_IMAGERY_PATH": "preprocessed/annotated_imagery",
+            "FINAL_DATA_PATH": "preprocessed/final",
+            "PREGENERATED_GEOLOCATION_PATH": "preprocessed/geolocation",
+            # Scratch Directories
+            "SCRATCH": "scratch",
+            "LOCALSCRATCH": "scratch",
+            "SHAREDSCRATCH": "scratch",
+            # Log and Data Directories
+            "LOGDIR": "geoipsdata",
+            "GEOIPSDATA": "geoipsdata",
+            # Ancillary Data Directories
+            "GEOIPS_ANCILDAT_AUTOGEN": "ancildat_autogen",
+            "GEOIPS_ANCILDAT": "ancildat",
+            # WWW Paths
+            "TCWWW": "preprocessed/tcwww",
+            "PUBLICWWW": "preprocessed/publicwww",
+            "PRIVATEWWW": "preprocessed/privatewww",
+            # Tropical Cyclone Paths
+            "TC_DECKS_DB": "longterm_files/tc/tc_decks.db",
+            "TC_DECKS_DIR": "longterm_files/tc/decks",
+        },
+        paths["BASE_PATH"]: {
+            "TC_TEMPLATE": [
+                "plugins/yaml/sectors/dynamic/tc_web_template.yaml",
+            ],
+        },
+    }
 
-    # Configuration and Queue
-    paths["GEOIPS_RCFILE"] = os.getenv("GEOIPS_RCFILE", "")
-    paths["DEFAULT_QUEUE"] = os.getenv("DEFAULT_QUEUE", None)
+    for top_directory, sub_directories in derivative_directory_paths.items():
+        for key, sub_path in sub_directories.items():
+            paths[key] = os.path.join(top_directory, sub_path)
 
-    # Hostname and Home Directory
-    paths["BOXNAME"] = socket.gethostname()
     if not os.getenv("HOME"):
         # need home drive default for windows
         paths["HOME"] = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH")
     else:
         paths["HOME"] = os.getenv("HOME").rstrip("/")
 
-    # Scratch Directories
-    scratch_dir = get_env_path(
-        "SCRATCH", os.path.join(paths["GEOIPS_OUTDIRS"], "scratch")
-    )
-    paths["SCRATCH"] = scratch_dir
-    paths["LOCALSCRATCH"] = get_env_path("LOCALSCRATCH", scratch_dir)
-    paths["SHAREDSCRATCH"] = get_env_path("SHAREDSCRATCH", scratch_dir)
-
-    # Ancillary Data Directories
-    paths["GEOIPS_ANCILDAT"] = get_env_path(
-        "GEOIPS_ANCILDAT", os.path.join(paths["GEOIPS_OUTDIRS"], "ancildat")
-    )
-    paths["GEOIPS_ANCILDAT_AUTOGEN"] = get_env_path(
-        "GEOIPS_ANCILDAT_AUTOGEN",
-        os.path.join(paths["GEOIPS_OUTDIRS"], "ancildat_autogen"),
-    )
-
-    # Log and Data Directories
-    paths["LOGDIR"] = get_env_path(
-        "LOGDIR", os.path.join(paths["GEOIPS_OUTDIRS"], "logs")
-    )
-    paths["GEOIPSDATA"] = get_env_path(
-        "GEOIPSDATA", os.path.join(paths["GEOIPS_OUTDIRS"], "geoipsdata")
-    )
-
-    # Version
-    paths["GEOIPS_VERS"] = os.getenv("GEOIPS_VERS", "0.0.0")
-
-    # WWW Paths
-    www_paths = {
-        "TCWWW": "preprocessed/tcwww",
-        "PUBLICWWW": "preprocessed/publicwww",
-        "PRIVATEWWW": "preprocessed/privatewww",
-    }
-
-    for key, subdir in www_paths.items():
-        paths[key] = get_env_path(key, os.path.join(paths["GEOIPS_OUTDIRS"], subdir))
-        paths[f"{key}_URL"] = get_env_path(f"{key}_URL", paths[key])
-
-    # Tropical Cyclone Paths
-    paths["TC_DECKS_DB"] = get_env_path(
-        "TC_DECKS_DB",
-        os.path.join(paths["GEOIPS_OUTDIRS"], "longterm_files/tc/tc_decks.db"),
-    )
-    paths["TC_DECKS_DIR"] = get_env_path(
-        "TC_DECKS_DIR", os.path.join(paths["GEOIPS_OUTDIRS"], "longterm_files/tc/decks")
-    )
-
-    # Template Paths
-    paths["TC_TEMPLATE"] = get_env_path(
-        "TC_TEMPLATE",
-        os.path.join(
-            paths["BASE_PATH"], "plugins/yaml/sectors/dynamic/tc_web_template.yaml"
-        ),
-    )
+    # Setting links for WWW Paths
+    www_paths = ["TCWWW", "PUBLICWWW", "PRIVATEWWW"]
+    for path in www_paths:
+        paths[f"{path}_URL"] = get_env_var(f"{path}_URL", paths[path])
 
     return paths
 
