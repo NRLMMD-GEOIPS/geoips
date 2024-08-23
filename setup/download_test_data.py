26c87e53bd1b06713560b9a940f8d91c6a52bb0c
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-08-23 09:51:31 -0600

    Improve downloading functionality; add argparse

--- setup/download_test_data.py
+++ setup/download_test_data.py
@@ -4,32 +4,89 @@
 # # # https://github.com/NRLMMD-GEOIPS.
 
 """Download data from a specified URL."""
+import subprocess
+import requests
+import tarfile
+import argparse
+from rich.console import Console
+from rich.progress import Progress
 
-import sys
-import urllib
-import tempfile
-from subprocess import check_output
-import urllib.request
+# Initialize console with a default value
+console = None
+use_rich = True
 
 
-def download_test_data(url, dest=None):
-    """Download the specified URL and write to stdout as bytes.
+def setup_rich_console(use_rich):
+    global console
+    console = Console() if use_rich else None
 
-    Will raise requests.exceptions.HTTPError on failure.
-    """
-    if ".git" in url:
-        print(f"git clone {url} {dest}")
-        check_output(["git", "clone", url, dest])
-        print("done git clone")
+
+def output_to_console(message, style=None):
+    if console:
+        console.print(message, style=style)
     else:
-        with tempfile.NamedTemporaryFile(delete_on_close=False) as f:
-            with urllib.request.urlopen(url) as url_stream:
-                urllib.request.urlretrieve(url_stream, f)
-            print(f.name)
+        print(message)
+
+
+def download_from_git(repo_url, destination):
+    try:
+        output_to_console(
+            f"Cloning repository from {repo_url} to {destination}...", style="bold cyan"
+        )
+        subprocess.check_output(["git", "clone", repo_url, destination])
+        output_to_console("Repository successfully cloned.", style="bold green")
+    except subprocess.CalledProcessError as e:
+        output_to_console(
+            f"Failed to clone repository: {e.output.decode('utf-8')}", style="bold red"
+        )
+
+
+def download_and_extract_compressed_tar(url, dest, comp="gz"):
+    output_to_console(
+        f"Downloading and extracting {url} to {dest}...", style="bold cyan"
+    )
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            file_length = int(r.headers.get("content-length", 0))
+            with open(dest, "wb") as f:
+                with tarfile.open(fileobj=r.raw, mode=f"r|{comp}") as tar:
+                    output_to_console(
+                        "Downloading and extracting {file_length} bytes... ",
+                        style="cyan",
+                    )
+                    tar.extractall(path=dest)
+        output_to_console("Files successfully downloaded and extracted.", style="green")
+    except Exception as e:
+        output_to_console(f"Failed to download or extract files.", style="bold red")
+        raise e
+
+
+def main():
+    global use_rich
+    parser = argparse.ArgumentParser(description="Download test data for GeoIPS")
+    parser.add_argument("url", help="The URL to the .tgz file.")
+    parser.add_argument("output_dir", help="The directory to extract files to.")
+    parser.add_argument(
+        "--no-rich",
+        action="store_true",
+        help="Disable rich text formatting and progress bars.",
+    )
+
+    args = parser.parse_args()
+
+    use_rich = not args.no_rich
+    setup_rich_console(use_rich)
+
+    if ".git" in args.url:
+        download_from_git(args.url, args.output_dir)
+    elif ".tgz" in args.url:
+        download_and_extract_compressed_tar(args.url, args.output_dir)
+    else:
+        output_to_console(
+            "Error: Cannot handle non-git non-tgz urls.", style="bold red"
+        )
 
 
 if __name__ == "__main__":
-    dest = None
-    if len(sys.argv) == 3:
-        dest = sys.argv[2]
-    download_test_data(sys.argv[1], dest)
+    main()
