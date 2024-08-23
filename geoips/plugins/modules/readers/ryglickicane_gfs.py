e046ba7405a059c634de6a240929247fa65edb78
Andrew Thorpe <andrew.thorpe.ctr@nrlmry.navy.mil>
2024-08-23 23:54:09 +0000

    Single layer reads

--- geoips/plugins/modules/readers/ryglickicane_gfs.py
+++ geoips/plugins/modules/readers/ryglickicane_gfs.py
@@ -14,10 +14,18 @@ import pygrib as pyg
 import numpy as np
 
 
+surface_vars = ['Convective available potential energy','Convective inhibition','Temperature',
+                'Snow depth','Ice thickness','Precipitation rate','Vegetation','Sunshine Duration',
+                'Sea ice temperature','Sea ice area fraction','Wind speed (gust)']
+# CAPE, CIN, and PWAT
 tmp_attrs = ['units','pressureUnits']
+surf_attrs = ['units','shortName',
+              'name','typeOfLevel']
 
-def grb_to_xr(grb_obj):
-    """Takes in preselected grb values."""
+
+
+def press_grb_to_xr(grb_obj):
+    """Converts pressure grb values to xarray.Dataset."""
     # get unq levels
     lvls = np.asarray(sorted(set((i['level'] for i in grb_obj))))
     lats, lons = grb_obj[0].latlons()
@@ -41,28 +49,67 @@ def grb_to_xr(grb_obj):
     return varlist
         
 
+def surf_grb_to_xr(grb_obj):
+    """Converts surface grb values to xarray.Dataset"""
+    varlist = {}
+
+    lats, lons = grb_obj[0].latlons()
+
+    for i in grb_obj:
+        tmp_key = '_'.join(i.name.lower().split(' '))
+
+        data_attrs = {k:i[k] for k in surf_attrs}
+
+        varlist[tmp_key] = xr.DataArray(data=i.values,
+                                        dims=('xi','yi'),
+                                        attrs=data_attrs)
+
+    varlist['lat'] = xr.DataArray(lats,dims=('xi','yi'))
+    varlist['lon'] = xr.DataArray(lons,dims=('xi','yi'))
+    return None
+
 
 def read_atmos(filenames):
     """Read gfs common parameters.
     """
     print("Reading ATMOS file")
-    xr_list = []
+    xr_list = {'gfs_pressure':[],'gfs_surface':[],'gfs_singlelayer':[]}
+    base_time = []
+    fcst_time = []
     for f in filenames:
         # read each file
-
-        # 2 second read, yikes
         pg_frame = pyg.open(f)
 
         # Pressure
         tmp_pres = pg_frame.select(typeOfLevel='isobaricInhPa')
-        xr_pres = grb_to_xr(tmp_pres)
+        xr_pres = press_grb_to_xr(tmp_pres)
 
-        xr_list.append(xr_pres)
+        # Surface
+        tmp_surf = pg_frame.select(name=surface_vars,
+                                   typeOfLevel='surface')
+        surface_xr = surf_grb_to_xr(tmp_surf)
 
-        
+        # Single Layer, is ok to considerd as surface (slab)
+        tmp_sl = pg_frame.select(typeOfLevel='atmosphereSingleLayer')
+        single_atm_xr = surf_grb_to_xr(tmp_sl)
 
+        xr_list['gfs_pressure'].append(xr_pres)
+        xr_list['gfs_surface'].append(surface_xr)
+        xr_list['gfs_singlelayer'].append(single_atm_xr)
 
-    return None
+        # track the analysis and valid forcast date
+        base_time.append(tmp_sl[0].analDate)
+        fcst_time.append(tmp_sl[0].validDate)
+
+    if set(base_time) > 1:
+        # one set time with multiple forcasts
+        LOG.info("One forcast time")
+        base_time = base_time[0]
+    else:
+        LOG.info("Multiple base forcast times, attempting merge")
+    # track valid and analysis date
+
+    return xr_list
 
 def read_wave(filenames):
     """Read gfswave products.
