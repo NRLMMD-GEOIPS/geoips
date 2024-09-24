bcdb5e3e44b2e6a9789952b14ac2c68d930a291e
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-09-24 06:42:18 -0400

    removed redundant imports, optimized, & organized

--- geoips/plugins/modules/readers/ssmis_binary.py
+++ geoips/plugins/modules/readers/ssmis_binary.py
@@ -18,12 +18,10 @@ Output Fields
      XARRAY onjectives to hold variables
 """
 # Python Standard Libraries
+from datetime import datetime, timedelta
 import logging
 from os.path import basename
 
-# import os
-from datetime import datetime, timedelta
-import os
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -117,13 +115,13 @@ def append_xarray_dicts(xobjs_list):
 
 def read_ssmis_data_file(fname, metadata_only=False):
     """Read a single SSMIS data file."""
-    data_name = os.path.basename(fname).split("_")[-1].split(".")[-1]
+    data_name = basename(fname).split("_")[-1].split(".")[-1]
 
     if data_name != "raw":
         LOG.warning("Warning: wrong SSMI SDR data type:  data_type=%s", data_name)
         raise
 
-    if "cfnoc" in os.path.basename(fname) and "sdris" in os.path.basename(fname):
+    if "cfnoc" in basename(fname) and "sdris" in basename(fname):
         LOG.info("found a SSMIS SDR file")
     else:
         LOG.info("not a SSMIS SDR file: skip it")
@@ -242,7 +240,7 @@ def read_ssmis_data_file(fname, metadata_only=False):
             # on the header information in the binary file
 
             # Grab only the file name
-            bfname = os.path.basename(fname)
+            bfname = basename(fname)
 
             # Standard SSMIS file names are the following format:
             # <FILE_ID>_<SATELLITE>_<DATE>_<START>_<END>_<REV>_<EXTENSION>
@@ -275,7 +273,7 @@ def read_ssmis_data_file(fname, metadata_only=False):
             # on the header information in the binary file
 
             # Grab only the file name
-            bfname = os.path.basename(fname)
+            bfname = basename(fname)
 
             # Standard SSMIS file names are the following format:
             # <FILE_ID>_<SATELLITE>_<DATE>_<START>_<END>_<REV>_<EXTENSION>
@@ -308,7 +306,7 @@ def read_ssmis_data_file(fname, metadata_only=False):
             # on the header information in the binary file
 
             # Grab only the file name
-            bfname = os.path.basename(fname)
+            bfname = basename(fname)
 
             # Standard SSMIS file names are the following format:
             # <FILE_ID>_<SATELLITE>_<DATE>_<START>_<END>_<REV>_<EXTENSION>
