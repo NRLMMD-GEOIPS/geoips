a0b2d50a5a88be398d5fe6f9b55788c906ae7ef7
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-10-04 04:51:49 -0400

    valid pydanitc model classes & working data validation

--- geoips/plugins/modules/procflows/order_based.py
+++ geoips/plugins/modules/procflows/order_based.py
@@ -1,46 +1,45 @@
 from argparse import ArgumentParser
 import yaml
-from geoips.pydantic.products import ProductPlugin
-from geoips import interfaces
+
 from IPython import embed as shell
 
+from geoips.pydantic.products import ProductPlugin
+from geoips import interfaces
 # from geoips.interfaces import products
-#
+
+
+# from geoips.interfaces import products
 # products.get_plugin(product_name)
-
-
 interface = "procflows"
 family = "standard"
 name = "order_based"
-
 
 def call(fnames: list[str], product_path: str, command_line_args: list[str] | None = None) -> None:
     """
     runs an order-based procflow processing with the specified input data files & product definition file
     
         Parameters:
-            fnames (List[str]): list of filenames to process
+            fnames (list[str]): list of filenames to process
             product-path (str): path to the product definition file
+            command_line_args (list[str] | None, optional): fnames & product-path via CLI
 
         Returns:
             None
     """
-    with open(product_path) as f:
-        prod_dict = yaml.safe_load(f)
+    with open(product_path) as product_definition_file:
+        prod_dict = yaml.safe_load(product_definition_file)
         prod = ProductPlugin(**prod_dict)
+        print(prod)
+    
+    print(f"interface: {prod.get_interface()}")
+    print(f"reader: {prod.get_reader()}")
+    print(f"variables: {prod.get_reader_variables()}")
+    
+    interface = prod.get_interface()
+    # reader = interfaces.products.get_plugin(prod.get_interface())
+    # reader = interface.get_plugin(step.reader)
 
-    for step in prod.spec.steps:
-        print(step.interface)
-        print(step.reader)
-        print(step.arguments)
-
-        # Maybe we should add a "get_interface" function rather than requiring
-        # `getattr`?
-        interface = getattr(interfaces, step.interface)
-
-        reader = interface.get_plugin(step.reader)
-        shell()
-
+    
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="order-based procflow processing")
--- geoips/pydantic/products.py
+++ geoips/pydantic/products.py
@@ -1,7 +1,8 @@
 """Product plugin format."""
 
-from typing import Literal
-from pydantic import BaseModel, Field
+from typing import Literal, Union
+from pydantic import BaseModel, Field, root_validator
+
 from geoips.pydantic.bases import Plugin
 
 
@@ -12,48 +13,71 @@ class ReaderArguments(BaseModel):
         None, description="The variables to read from the input file."
     )
 
+class ReaderInterfaceStep(BaseModel):
+    """A step to read interface name for the reader"""
+    interface: Literal["readers"] = Field(
+        default="readers", 
+        description="Interface name, must be 'readers'",
+        frozen=True
+    )
+
 
 class ReaderStep(BaseModel):
     """A step to read data from a file."""
-
-    # I'm not sure that I like this syntax. I also am not sure that I like the error
-    # that it generates when someone attempts to set this to a different value.
-    # When set to a different value, the error raised is:
-    #
-    #    ValidationError: 1 validation error for Mine
-    #    interface
-    #      Input should be 'readers' [type=literal_error, input_value='SomeoneElse', input_type=str]
-    #        For further information visit
-    #        https://errors.pydantic.dev/2.8/v/literal_error
-    #
-    # Another option would be to use typing.ClassVar but that would hide the attribute
-    # from the model. It would still be accessible as a class attribute but it would not
-    # be visible in the model's documentation, it's string representation, etc.
-    #
-    # Maybe this could be set on a base ProductStep class to validate against the name
-    # of the class or against a ClassVar.
-    #
-    # I'm definitely open to solutions here.
-    #
-    # The behavior that I'd like to see is that the interface attribute defaults to
-    # "readers" and is not required. If it is set, it must be set to "readers". If it is
-    # set to a different value it should raise an error that is more informative than
-    # the one that is currently raised.
-    interface: Literal["readers"] = "readers"
-
     reader: str = Field(description="The name of the reader plugin to be used.")
     arguments: ReaderArguments = Field(
         description="The arguments to pass to the reader plugin."
     )
 
-
 class ProductSpec(BaseModel):
     """The specification for a product."""
 
-    steps: list[ReaderStep] = Field(description="The steps to produce the product.")
+    steps: list[Union[ReaderInterfaceStep, ReaderStep]] = Field(description="The steps to produce the product.")
+
+    @root_validator(pre=True)
+    def ensure_interface_step(cls, values):
+        """Ensure that 'interface: readers' step is present if not explicitly provided."""
+        steps = values.get('steps', [])
+        
+        # Check if there is no 'interface: readers' step and insert it as the first step if missing
+        if not any(isinstance(step, dict) and step.get('interface') == 'readers' for step in steps):
+            # Insert 'interface: readers' step as the first step
+            steps.insert(0, {"interface": "readers"})
+        
+        values['steps'] = steps
+        return values
+
+    def get_interface(self):
+        for step in self.steps:
+            if hasattr(step, 'interface'):
+                return step.interface
+    
+    def get_reader(self):
+        for step in self.steps:
+            if hasattr(step, 'reader'):
+                return step.reader
+    
+    def get_reader_variables(self):
+        for step in self.steps:
+            if hasattr(step, 'arguments'):
+                return step.arguments.variables
 
 
 class ProductPlugin(Plugin):
     """A plugin that produces a product."""
 
     spec: ProductSpec = Field(description="The product specification")
+
+    def get_interface(self):
+        return self.spec.get_interface()
+
+    def get_reader(self):
+        return self.spec.get_reader()
+
+    def get_reader_variables(self):
+        return self.spec.get_reader_variables()
+        
+    
+
+
+
