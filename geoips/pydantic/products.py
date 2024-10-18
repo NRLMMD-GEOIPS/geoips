95b2f2e599f6f7859f2e04e5aa56e8e00c64e009
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-10-18 13:10:57 -0400

    bugfix: Literal[*get_interface_names()] none return type bug

--- geoips/pydantic/products.py
+++ geoips/pydantic/products.py
@@ -2,6 +2,7 @@
 
 from typing import Literal, Union
 from pydantic import BaseModel, Field, root_validator
+from geoips import interfaces
 
 from geoips.pydantic.bases import Plugin
 
@@ -13,71 +14,44 @@ class ReaderArguments(BaseModel):
         None, description="The variables to read from the input file."
     )
 
-class ReaderInterfaceStep(BaseModel):
-    """A step to read interface name for the reader"""
-    interface: Literal["readers"] = Field(
-        default="readers", 
-        description="Interface name, must be 'readers'",
-        frozen=True
-    )
+
+class AlgorithmArguments(BaseModel):
+    pass
 
 
-class ReaderStep(BaseModel):
-    """A step to read data from a file."""
-    reader: str = Field(description="The name of the reader plugin to be used.")
-    arguments: ReaderArguments = Field(
-        description="The arguments to pass to the reader plugin."
-    )
+class OutputFormatterArguments(BaseModel):
+    """output_formatters"""
+    pass
+
+def get_interface_names():
+    interface_names = []
+    for ifs in interfaces.list_available_interfaces().values():
+        interface_names.extend(ifs)
+    return interface_names
+    
+class Step(BaseModel):
+    interface: Literal[*get_interface_names()]
+    name: str
+    arguments: Union[ReaderArguments, AlgorithmArguments]
+
+    @root_validator(pre=True)
+    def validate_arguments(cls, values):
+        interface_name = values.get("interface")
+        interface_camel_case = "".join(
+            [word.caplitalize() for word in interface_name[:-1].split("_")]
+        )
+        interface_arguments_model_name = f"{interface_camel_case}Arguments"
+        interface_arguments_model = globals().get(interface_arguments_model_name)
+        interface_arguments_model(**values.get("arguments", {}))
+
 
 class ProductSpec(BaseModel):
     """The specification for a product."""
 
-    steps: list[Union[ReaderInterfaceStep, ReaderStep]] = Field(description="The steps to produce the product.")
-
-    @root_validator(pre=True)
-    def ensure_interface_step(cls, values):
-        """Ensure that 'interface: readers' step is present if not explicitly provided."""
-        steps = values.get('steps', [])
-        
-        # Check if there is no 'interface: readers' step and insert it as the first step if missing
-        if not any(isinstance(step, dict) and step.get('interface') == 'readers' for step in steps):
-            # Insert 'interface: readers' step as the first step
-            steps.insert(0, {"interface": "readers"})
-        
-        values['steps'] = steps
-        return values
-
-    def get_interface(self):
-        for step in self.steps:
-            if hasattr(step, 'interface'):
-                return step.interface
-    
-    def get_reader(self):
-        for step in self.steps:
-            if hasattr(step, 'reader'):
-                return step.reader
-    
-    def get_reader_variables(self):
-        for step in self.steps:
-            if hasattr(step, 'arguments'):
-                return step.arguments.variables
+    steps: Step = Field(description="The steps to produce the product.")
 
 
 class ProductPlugin(Plugin):
     """A plugin that produces a product."""
 
     spec: ProductSpec = Field(description="The product specification")
-
-    def get_interface(self):
-        return self.spec.get_interface()
-
-    def get_reader(self):
-        return self.spec.get_reader()
-
-    def get_reader_variables(self):
-        return self.spec.get_reader_variables()
-        
-    
-
-
-
