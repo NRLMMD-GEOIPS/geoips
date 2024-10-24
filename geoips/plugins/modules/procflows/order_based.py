7b0894f96bf698248f4638462093e9b96d60f79f
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-10-24 12:12:00 -0400

    initial working after fixing root_validator bug and clean up

--- geoips/plugins/modules/procflows/order_based.py
+++ geoips/plugins/modules/procflows/order_based.py
@@ -1,1 +1,51 @@
 """Processing workflow for order based data source processing."""
+
+from argparse import ArgumentParser
+import yaml
+
+
+from geoips.pydantic.products import ProductPlugin
+
+
+interface = "procflows"
+family = "standard"
+name = "order_based"
+
+
+def call(
+    fnames: list[str], product_path: str, command_line_args: list[str] | None = None
+) -> None:
+    """runs an order-based procflow processing with the specified input data files &
+    steps listed in product definition file (PDF)
+
+    Parameters
+    ----------
+        * fnames (list[str]): list of filenames to process
+        * product-path (str): path to the product definition file
+        * command_line_args (list[str] | None, optional): fnames & product-path
+
+    Returns
+    -------
+        * None
+    """
+    with open(product_path) as product_definition_file:
+        prod_dict = yaml.safe_load(product_definition_file)
+        prod = ProductPlugin(**prod_dict)
+        print("prod is \t", prod)
+
+    for step in prod.spec.steps:
+        print(f"\n\nstep\t {step}")
+        # interface_name = step.interface
+        # print(f"interface type in OB: {step.interface}")
+        print(f"plugin name is {step.name}")
+        print(f"plugin arguments are {step.arguments}")
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description="order-based procflow processing")
+    parser.add_argument("fnames", nargs="+", help="The filenames to process.")
+    parser.add_argument("-p", "--product_path", help="The path to the PDF.")
+
+    args = parser.parse_args()
+    call(args.fnames, args.product_path)
--- geoips/pydantic/products.py
+++ geoips/pydantic/products.py
@@ -1,1 +1,60 @@
 """Product plugin format."""
+
+from pydantic import BaseModel, Field, model_validator
+from typing import Any, Dict, List, Literal
+
+from geoips import interfaces
+from geoips.pydantic.bases import Plugin
+
+
+def get_plugin_types():
+    """Generates list of possible plugin types."""
+    interface_types = []
+    for ifs in interfaces.list_available_interfaces().values():
+        interface_types.extend(ifs)
+    return list(set(interface[:-1] for interface in interface_types))
+
+
+class Step(BaseModel):
+    """Lists sequence of steps along with the step data."""
+
+    type: Literal[*get_plugin_types()] = Field(description="interface type")
+    name: str
+    arguments: Dict[str, Any] = Field(
+        default_factory=dict, description="Arguments for the step."
+    )
+
+    @model_validator(mode='before')
+    def validate_arguments(cls, values):
+
+        interface_type, plugin_data = next(iter(values.items()))
+
+        valid_interfaces = get_plugin_types()
+        if interface_type not in get_plugin_types():
+            raise ValueError(
+                f"Invalid interface type: {interface_type}"
+                f"\ninterface must be one of \n\t: {valid_interfaces} \n\n"
+            )
+
+        print(f"interface type : {interface_type}")
+        values["type"] = interface_type
+        values["name"] = plugin_data.get("name", "")
+        print("plugin name : ", values["name"])
+        values["arguments"] = plugin_data.get("arguments", {})
+        print("plugin arguments : ", values["arguments"])
+        print("\n\n")
+        # values.update(plugin_data)
+        return values
+
+
+class ProductSpec(BaseModel):
+    """The specification for a product."""
+
+    # list of steps
+    steps: List[Step] = Field(description="The steps to produce the product.")
+
+
+class ProductPlugin(Plugin):
+    """A plugin that produces a product."""
+
+    spec: ProductSpec = Field(description="The product specification")
