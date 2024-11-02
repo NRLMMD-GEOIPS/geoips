83a71928bb115ed33eadee636b755a45c3a6274a
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-11-02 00:22:13 -0400

    minor tweaks

--- geoips/plugins/modules/procflows/order_based.py
+++ geoips/plugins/modules/procflows/order_based.py
@@ -41,17 +41,28 @@ def call(
         step_def = step.definition
         print(
             # f"\n\nstep\t {step} \n\nplugin \n\t"
+            # f"\n\t step_name : {step_def.step_name}"
             f"\n\t type : {step_def.type}"
             f"\n\t name : {step_def.name} \n\t arguments : {step_def.arguments}"
         )
 
         interface = step_def.type + "s"
-        plugin_instance = getattr(interfaces, interface, None).get_plugin(step_def.name)
 
-        print(
-            f"\n\n{step_def.type} processing details:\n\n\t"
-            f"{plugin_instance(fnames, step_def.arguments)}\n\n"
-        )
+        # the if-else ladder will go away after all steps are implemented
+        plugin_instance = getattr(interfaces, interface, None).get_plugin(step_def.name)
+        if interface == "readers":
+            print(
+                f"\n\n{step_def.type} processing details:\n\n\t"
+                f"{plugin_instance(fnames, step_def.arguments)}\n\n"
+            )
+        elif interface == "algorithms":
+            pass
+        elif interface == "interpolators":
+            pass
+        elif interface == "output_formatter":
+            pass
+        else:
+            pass
 
 
 if __name__ == "__main__":
