6a74707cb184145bda10b10ff4d2863733eb869a
BSL-5 <biosafetylevel5@gmail.com>
2024-11-12 21:51:09 +0000

    update ci

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,8 +2,8 @@
 # # # https://github.com/NRLMMD-GEOIPS.
 
 #!/bin/sh
-echo " WARNING: This script has been deprecated. Please start calling build_docs.py instead"
-python3 $GEOIPS_PACKAGES_DIR/docs/build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir $GEOIPS_PACKAGES_DIR/$2/build $1 $2
+echo "WARNING: This script has been deprecated. Please start calling build_docs.py instead"
+python3 $GEOIPS_PACKAGES_DIR/geoips/docs/build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir $GEOIPS_PACKAGES_DIR/$2/build $1 $2
 
 
 
