705c46b9405875a73044af949f6721622524b594
BSL-5 <biosafetylevel5@gmail.com>
2024-11-12 21:36:53 +0000

    fix script call

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,7 +2,7 @@
 # # # https://github.com/NRLMMD-GEOIPS.
 
 #!/bin/sh
-python3 build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir $GEOIPS_PACKAGES_DIR/$2/build $1 $2
+python3 $GEOIPS_PACKAGES_DIR/docs/build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir $GEOIPS_PACKAGES_DIR/$2/build $1 $2
 
 
 
