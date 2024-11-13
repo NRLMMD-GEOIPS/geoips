40deefae7177f3ae5fbc50ee8b719a6381bfbd9e
BSL-5 <biosafetylevel5@gmail.com>
2024-11-13 22:33:54 +0000

    update CI

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -3,7 +3,7 @@
 
 #!/bin/sh
 echo "WARNING: This script has been deprecated. Please start calling build_docs.py instead"
-python3 $GEOIPS_PACKAGES_DIR/geoips/docs/build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir $GEOIPS_PACKAGES_DIR/$2/docs/build/sphinx/html $1 $2
+python3 $GEOIPS_PACKAGES_DIR/geoips/docs/build_docs.py --license-url $GEOIPS_REPO_URL --geoips-docs-path $GEOIPS_PACKAGES_DIR/$2/docs --output-dir "$GEOIPS_PACKAGES_DIR/$2/docs/build/sphinx/html" $1 $2
 
 
 
