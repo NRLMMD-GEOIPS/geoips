09dbb1f2c350b49b756266fea51e9ea58b6c106c
srikanth-kumar <srikanthkumargampa@gmail.com>
2024-12-16 23:04:30 -0500

    minor tweaks

--- geoips/pydantic/__init__.py
+++ geoips/pydantic/__init__.py
@@ -1,1 +1,4 @@
-from geoips.pydantic.bases import PluginModel
+# # # This source code is protected under the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+"""Geoips order-based procflow pydantic models init file."""
