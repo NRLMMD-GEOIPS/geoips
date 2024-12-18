fc21bcdc10ea4a12472a7a57c977b0af9edfe719
mindyls <13873624+mindyls@users.noreply.github.com>
2024-12-18 08:37:47 -0800

    Update build_docs.sh

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -6,7 +6,8 @@ echo "WARNING: This script has been deprecated. Please start calling build_docs.
 reponame=$2
 repopath=$1
 save_temp_dir=$GEOIPS_OUTDIRS/scratch/docs/$reponame
-python3 $GEOIPS_PACKAGES_DIR/geoips/docs/build_docs.py \
+curr_path=`dirname $0`
+python3 $curr_path/build_docs.py \
   --license-url $GEOIPS_REPO_URL \
   --geoips-docs-path $GEOIPS_PACKAGES_DIR/geoips/docs/ \
   --output-dir "$GEOIPS_PACKAGES_DIR/$2/docs/build/sphinx/html" \
