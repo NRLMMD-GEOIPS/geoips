7b9c21a6e7634b3297e5feef1784d95be9261296
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-01-23 01:28:22 +0000

    adding PrettyBaseModel

--- geoips/pydantic/standards.py
+++ geoips/pydantic/standards.py
@@ -6,10 +6,25 @@
 from pydantic import BaseModel, ConfigDict
 
 
+class PrettyBaseModel(BaseModel):
+    """Make Pydantic models pretty-print by default."""
+
+    def __str__(self) -> str:
+        """Return a pretty-print string representation of a Pydantic model.
+
+        The returned string will be formatted as JSON with two-space indentation.
+
+        Returns
+        -------
+            str: A string representation of the Pydantic model.
+        """
+        return self.model_dump_json(indent=2)
+
+
 class StandardBaseModel(BaseModel):
     """Standard Base Model for Pydantic validation in GeoIPS."""
 
-    model_config = ConfigDict(extra='allow')
+    model_config = ConfigDict(extra="allow")
 
 
 class Model(StandardBaseModel):
