edd0930fb4f1201fa0c234f1d68b4ae460e82065
BSL-5 <biosafetylevel5@gmail.com>
2025-02-03 19:08:17 +0000

    change mark

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,4 +1,4 @@
 echo "WARNING:"
 echo "\tWe are in the process of deprecating shell script based tests. Please switch to calling this test via 'pytest -m \"extra\"'"
 
-pytest -m "extra"
+pytest -m "integration"
