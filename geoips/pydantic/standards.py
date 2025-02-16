a4d194fbe5d95216503682aaad83aedcbd9c1445
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-02-16 14:54:29 -0500

    feature additons

--- geoips/pydantic/standards.py
+++ geoips/pydantic/standards.py
@@ -3,24 +3,27 @@
 
 """Pydantic Standard Base Model for GeoIPS."""
 
-from pydantic import BaseModel, ConfigDict, model_validator
+# Python Standard Libraries
+import keyword
+import logging
+
+# Third-Party Libraries
+from pydantic import BaseModel, ConfigDict, Field, model_validator
+from pydantic.functional_validators import AfterValidator
 from typing import ClassVar
+from typing_extensions import Annotated
 
 
-class StaticBaseModel(BaseModel):
-    """Static Base Model for Pydantic validation in GeoIPS."""
-
-    model_config = ConfigDict(
-        extra="allow", str_strip_whitespace=True, populate_by_name=True
-    )
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
 
 
-class DynamicBaseModel(BaseModel):
-    """Dynamic Base Model for Pydantic validation in GeoIPS."""
-
-    model_config = ConfigDict()
 class PrettyBaseModel(BaseModel):
-    """Make Pydantic models pretty-print by default."""
+    """Make Pydantic models pretty-print by default.
+
+    This model overrides the default string representation of Pyantic models to generate
+    a user-friendly, JSON-formatted output with two-space indentation.
+    """
 
     def __str__(self) -> str:
         """Return a pretty-print string representation of a Pydantic model.
@@ -29,15 +32,80 @@ class PrettyBaseModel(BaseModel):
 
         Returns
         -------
-            str: A string representation of the Pydantic model.
+        str
+            A JSON-formatted string representation of the Pydantic model.
         """
         return self.model_dump_json(indent=2)
 
 
-class StaticBaseModel(PrettyBaseModel):
-    """A pydantic model with a custom Pydantic ConfigDict."""
+def python_identifier(val: str) -> str:
+    """Validate if a string is a valid Python identifier.
 
-    model_config = ConfigDict(extra="forbid", populate_by_name=True)
+    Validate if a string is a valid Python identifier and not a reserved Python keyword.
+    `See <https://docs.python.org/3/reference/lexical_analysis.html#identifiers>`_ for
+    more information on Python identifiers and reserved keywords.
+
+    Validation is performed by calling `str.isidentifier` and `keyword.iskeyword`.
+
+    Parameters
+    ----------
+    val : str
+        The input string to validate.
+
+    Returns
+    -------
+    str
+        The input string if it is a valid Python identifier.
+
+    Raises
+    ------
+    ValueError
+        If the input string is invalid as a Python identifier or a reserved keyword.
+    """
+    error_messages = []
+    if not val.isidentifier():
+        error_messages.append(f"'{val}' is not a valid Python identifier.")
+    if keyword.iskeyword(val):
+        error_messages.append(f"'{val}' is a reserved Python keyword.")
+
+    if error_messages:
+        error_message = " ".join(error_messages) + " Please update it."
+        # LOG.interactive(error_message, exc_info=True)
+        LOG.info(error_message, exc_info=True)
+        raise ValueError(error_message)
+
+    return val
+
+
+# Create the PythonIdentifier type
+PythonIdentifier = Annotated[str, AfterValidator(python_identifier)]
+
+
+class StaticBaseModel(PrettyBaseModel):
+    """
+    Pydantic model with a customized ``ConfigDict`` configurations for GeoIPS.
+
+    This model extends ``PrettyBaseModel`` and uses Pydantic ConfigDict to provide
+    customized configurations such as forbidding extra fields.
+
+    # Attributes
+    # ----------
+    # model_config : ConfigDict
+    #     Configuration for the Pydantic model:
+    #     - `extra="forbid"`: Does not allow any additional fileds in the input data.
+    #     - `populate_by_name=True`: Enables populating fields by their aliases.
+    """
+
+    model_config = ConfigDict(
+        extra="allow", populate_by_name=True, str_strip_whitespace=True,
+        loc_by_alias=True
+    )
+
+    # sets the model name as the title
+    @property
+    def model_title(self):
+        return self.__class__.__name__
+
     has_disallowed_fields: ClassVar[bool] = False  # Default: no disallowed fields
     #  has_disallowed_fields = True
     #  set this in the inherited models, preferably before field definitions
@@ -113,19 +181,44 @@ class StaticBaseModel(PrettyBaseModel):
         return []
 
 
-class Model(StaticBaseModel):
-    """Model specifically meant for testing StandardBaseModel."""
+class Instrument(StaticBaseModel):
+    """Model for validating instrument information."""
 
-    x: str
-    y: str
+    sensor: str = Field(
+        ...,
+        min_length=1,
+        max_length=25,
+        pattern="^[A-Za-z0-9 _-]+$",
+        description="sensor_name",
+    )
+    spacecraft: str = Field(
+        default="GOES-R", description="The spacecraft associated with the instrument."
+    )
+    status: PythonIdentifier = Field(
+        ..., pattern="^(Active|Inactive|Testing|Decommissioned)$"
+    )
 
 
-# Won't strip white space from field values that are not members of the model
-# but are passed with extra=allow.
-# Won't strip the whitepsace if you pass the dictionary without initializing.
-# m = Model(x="hello ", y="world ")
+product = Instrument(
+    sensor="Advanced Baseline Imager", spacecraft="GOES-R  ", status="Active"
+)
+
+print(product.model_title)
+print(product.model_dump_json(indent=2))
+
+# print("Including only 'sensor' attribute using 'include' argument:")
+# print(ABI.model_dump_json(indent=2, include={"sensor"}))
+
+# print("Excluding 'sensor' attribute using 'exclude' argument")
+# print(ABI.model_dump_json(indent=2, exclude={"sensor"}))
 
 
-data = {"x": " hello", "y": "world "}
-model = Model(**data)
-print(model.model_dump())
+# class Instrument(StaticBaseModel):
+#     """Validate Instrument informaton."""
+
+#     name: str
+#     spacecraft: str
+
+
+# ABI = Instrument(name="Advanced Baseline Imager", y="GOES-R")
+# print(ABI.model_dump())
