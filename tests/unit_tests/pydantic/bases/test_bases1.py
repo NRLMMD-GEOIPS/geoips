019a978f31de8b4e99fc81dbfaeaf7d6f4d2ad1a
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-03-17 21:09:28 -0400

    test multi line description

---
+++ tests/unit_tests/pydantic/bases/test_bases1.py
@@ -0,0 +1,68 @@
+# # # This source code is protected under the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+"""Test Pydantic base models for Order-Based Procflow."""
+
+from copy import deepcopy
+from importlib.resources import files
+
+import pytest
+import yaml
+
+# from geoips.pydantic.sectors import SectorPluginModel
+from geoips.pydantic.bases import PluginModel
+from tests.unit_tests.pydantic.utils import (
+    PathDict,
+    load_test_cases,
+    validate_bad_plugin,
+    validate_good_plugin,
+)
+
+
+test_cases = load_test_cases("./test_pydantic_base_cases.yaml")
+print(f"test_cases is {test_cases}")
+good_yaml = yaml.safe_load(
+    open(str(files("geoips") / "plugins/yaml/workflows/read_test.yaml"), mode="r")
+)
+
+good_yaml["abspath"] = str(files("geoips") / "plugins/yaml/workflows/read_test.yaml")
+good_yaml["relpath"] = "plugins/yaml/workflows/read_test.yaml"
+good_yaml["package"] = "geoips"
+
+
+@pytest.fixture
+def good_plugin():
+    """Return a consistent dictionary that is a valid GeoIPS sector plugin."""
+    # Make the loading code only occur once, return a copy every time
+    return PathDict(deepcopy(good_yaml))
+
+
+@pytest.mark.parametrize(
+    "good_plugin",
+    [pytest.param("good_plugin", id="good_plugin")],
+    indirect=True,
+)
+def test_good_plugin(good_plugin):
+    """Assert that a well formatted sector plugin is valid.
+
+    Parameters
+    ----------
+    good_sector: dict
+        - A dictionary representing a valid sector plugin.
+    """
+    validate_good_plugin(good_plugin, PluginModel)
+
+
+@pytest.mark.parametrize("test_tup", test_cases.values(), ids=list(test_cases.keys()))
+def test_bad_plugins(good_plugin, test_tup):
+    """Perform validation on static sector plugins, including failing cases.
+
+    Parameters
+    ----------
+    good_sector: dict
+        - A dictionary representing a sector plugin that is valid.
+    test_tup:
+        - A tuple formatted (key, value, class, err_str), formatted (str, any, str, str)
+          used to run and validate tests.
+    """
+    validate_bad_plugin(good_plugin, test_tup, PluginModel)
