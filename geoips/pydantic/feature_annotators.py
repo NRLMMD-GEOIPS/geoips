64daf5cc7697d3e5be54a8e51c25cc4d882e032e
evrose54 <mrewrose@gmail.com>
2025-04-18 18:45:22 +0000

    updated documentation for ColorType

--- geoips/pydantic/feature_annotators.py
+++ geoips/pydantic/feature_annotators.py
@@ -19,8 +19,10 @@ class CartopyFeature(PermissiveFrozenModel):
     edgecolor: ColorType = Field(
         None,
         description=(
-            "A rgb tuple, named color, or hexidecimal string to apply to the edges of "
-            "the cartopy feature."
+            "A rgb tuple, matplotlib named color, or hexidecimal string (#XXXXXX) to "
+            "apply to the edges of the cartopy feature.\n"
+            "For more info, see: "
+            "https://matplotlib.org/stable/users/explain/colors/colors.html"
         ),
     )
     # NOTE: Once we add land / ocean features, we'll need to add another field, labeled
@@ -49,8 +51,10 @@ class FeatureAnnotatorSpec(FrozenModel):
     background: ColorType = Field(
         None,
         description=(
-            "A rgb tuple, named color, or hexidecimal string to apply to the background"
-            " of your image."
+            "A rgb tuple, matplotlib named color, or hexidecimal string (#XXXXXX) "
+            "to apply to the background of your image.\n"
+            "For more info, see: "
+            "https://matplotlib.org/stable/users/explain/colors/colors.html"
         ),
     )
 
@@ -61,7 +65,9 @@ class FeatureAnnotatorPluginModel(PluginModel):
     spec: FeatureAnnotatorSpec = Field(
         ...,
         description=(
-            "Specification of how to apply features to your annotated imagery. "
+            "Specification of how to apply cartopy features to your annotated imagery. "
             "Works alongside matplotlib and cartopy to generate these features."
+            "For more information, see: "
+            "https://scitools.org.uk/cartopy/docs/v0.14/matplotlib/feature_interface.html"  # NOQAx
         ),
     )
