e5ed87579728df2e9d1ce9b1f6571cbbe8236c2e
coleman-m <coleman.mcclelland@colostate.edu>
2025-04-22 13:14:51 -0600

    Fixing the test scripts to point to $GEOIPS_TESTDATA_DIR/test_data_awips_windspeed

--- tests/scripts/smos.awips.netcdf.sh
+++ tests/scripts/smos.awips.netcdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-geoips run single_source $GEOIPS_TESTDATA_DIR/test_data_smos/SM_OPER_MIR_SCNFSW_20240814T004732_20240814T022735_305_001_7.nc \
+geoips run single_source $GEOIPS_TESTDATA_DIR/test_data_awips_windspeed/SM_OPER_MIR_SCNFSW_20240814T004732_20240814T022735_305_001_7.nc \
 	--reader_name smos_winds_netcdf \
 	--product_name unsectored \
 	--output_formatter windspeed_awips2_formatter \
