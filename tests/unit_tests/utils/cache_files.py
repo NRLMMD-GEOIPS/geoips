a9e1911fed6d6aa8b1733db76b0e623ffa794f38
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2025-05-01 16:00:13 +0000

    Add tests for file caching

---
+++ tests/unit_tests/utils/cache_files.py
@@ -0,0 +1,101 @@
+import pytest
+
+import os
+import shutil
+import yaml
+import json
+
+from geoips.utils.cache_files import (
+    source_modified,
+    create_cached_json_from_yaml,
+    get_cached_json,
+)
+
+
+TMP_DIR = "/tmp/geoips_test"
+os.makedirs(TMP_DIR, exist_ok=True)
+
+# Don't create here, should be created by create_cached_json_from_yaml
+TMP_CACHE_DIR = "/tmp/geoips_test/cache"
+
+
+def test_source_modified():
+    """Test the source_modified function.
+
+    Create a temporary source and destination file, then test that source_modified
+    returns True if the source file is newer than the destination file, and False
+    otherwise. This will also test the case where the destination file does not exist.
+    """
+    # Create a temporary source and destination file
+    source = f"{TMP_DIR}/geoips_test_source.txt"
+    dest = f"{TMP_CACHE_DIR}/test_dest.txt"
+
+    with open(source, "w") as f:
+        f.write("This is a test.")
+
+    # Test that source_modified returns True if the destination file does not exist.
+    assert source_modified(source, dest)
+
+    os.makedirs(TMP_CACHE_DIR, exist_ok=True)
+    with open(dest, "w") as f:
+        f.write("This is a test.")
+
+    # Test that source_modified returns True if the source file is newer
+    os.utime(source, (os.path.getatime(source), os.path.getmtime(source) + 1))
+    assert source_modified(source, dest)
+
+    # Test that source_modified returns False if the destination file is newer
+    os.utime(dest, (os.path.getatime(dest), os.path.getmtime(dest) + 1))
+    assert not source_modified(source, dest)
+
+    # Clean up
+    os.remove(source)
+    os.remove(dest)
+    os.rmdir(TMP_CACHE_DIR)
+
+
+def test_create_cached_json_from_yaml():
+    """Test the create_cached_json_from_yaml function.
+
+    This will create a temporary YAML file, then call create_cached_json_from_yaml on
+    it. This will read the YAML file write the data to a JSON file in `cache_dir`. The
+    test just ensures that the JSON file was created and contains the correct data.
+    """
+    # Create a temporary YAML file
+    yaml_file = f"{TMP_DIR}/test.yaml"
+    with open(yaml_file, "w") as f:
+        yaml.dump({"key": "value"}, f)
+
+    # Create the cached JSON file
+    json_file = create_cached_json_from_yaml(yaml_file, cache_dir=TMP_CACHE_DIR)
+
+    # Check that the JSON file was created and contains the correct data
+    assert os.path.exists(json_file)
+    with open(json_file, "r") as f:
+        data = json.load(f)
+        assert data == {"key": "value"}
+
+    # Clean up
+    os.remove(yaml_file)
+    os.remove(json_file)
+
+
+def test_get_cached_json():
+    """Test the get_cached_json function.
+
+    Write a YAML file, then call get_cached_json on it. This will read the YAML file,
+    write it to a JSON file in `cache_dir`, read the JSON file and return the data, then
+    compare the new data to the original YAML data.
+    """
+    # Create a temporary YAML file
+    yaml_file = f"{TMP_DIR}/test.yaml"
+    with open(yaml_file, "w") as f:
+        yaml.dump({"key": "value"}, f)
+
+    # Get the cached JSON file
+    data = get_cached_json(yaml_file, cache_dir=TMP_CACHE_DIR)
+    assert data == {"key": "value"}
+
+    # Clean up
+    os.remove(yaml_file)
+    shutil.rmtree(TMP_CACHE_DIR)
