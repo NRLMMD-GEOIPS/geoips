fabaf2d19e7a0b5718e8adb645171051f7ab7760
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2025-05-01 20:31:09 +0000

    Fix bandit warning

--- tests/unit_tests/commandline/test_get_commandline_instructions.py
+++ tests/unit_tests/commandline/test_get_commandline_instructions.py
@@ -6,6 +6,7 @@
 import os
 from os.path import dirname, exists
 import pytest
+import tempfile
 
 from geoips.utils.cache_files import get_cached_json
 from geoips.commandline.commandline_interface import GeoipsCLI
@@ -14,11 +15,6 @@ from geoips.commandline.commandline_interface import GeoipsCLI
 # This points to the instructions directory for the unit tests. These are test
 # instructions that are not used in the actual codebase.
 INSTRUCT_DIR = f"{str(dirname(__file__))}/cmd_instructions"
-# The directory where JSON instructions are cached for unit tests. This differs from
-# default cache directory to avoid overwriting the actual cache directory.
-CACHE_DIR = "/tmp/geoips_test/cache"
-
-os.makedirs(CACHE_DIR, exist_ok=True)
 
 
 def generate_id(dir_name):
@@ -43,28 +39,35 @@ def test_instruction_cases(dir_name):
             - newer .yaml file than the .json file
             - newer .json file than the .yaml file
     """
+    # This will take the place of the user cache directory for the unit tests.
+    # This is where the cached JSON files will be stored.
+    # This is a temporary directory that will be deleted when the test is complete.
+    cache_dir = tempfile.mkdtemp()
+
     # Path to the directory containing the YAML instructions
     cmd_dir = f"{INSTRUCT_DIR}/{dir_name}"
     cmd_yaml = f"{cmd_dir}/cmd_instructions.yaml"
-    cmd_json = f"{CACHE_DIR}/cmd_instructions.json"
+    cmd_json = f"{cache_dir}/cmd_instructions.json"
 
     if "invalid" in dir_name:
         # Load the invalid instructions
         cmd_instructions = get_cached_json(
-            f"{cmd_dir}/cmd_instructions.yaml", cache_dir=CACHE_DIR
+            f"{cmd_dir}/cmd_instructions.yaml",
+            cache_dir=cache_dir,
         )
         # Ensure that a key error is raised
         GeoipsCLI(cmd_instructions=cmd_instructions)
     elif "missing" in dir_name:
         if dir_name == "json_missing":
-            cmd_instructions = get_cached_json(cmd_yaml, cache_dir=CACHE_DIR)
+            cmd_instructions = get_cached_json(cmd_yaml, cache_dir=cache_dir)
             assert exists(cmd_json)
         else:
             with pytest.raises(FileNotFoundError):
-                get_cached_json(cmd_yaml, cache_dir=CACHE_DIR)
+                get_cached_json(cmd_yaml, cache_dir=cache_dir)
 
-    # # Clean up
-    # try:
-    #     os.remove(cmd_json)
-    # except FileNotFoundError:
-    #     pass
+    # Clean up
+    try:
+        os.remove(cmd_json)
+    except FileNotFoundError:
+        pass
+    os.rmdir(cache_dir)
