5e3e49bfebef5fd4a6b957165dd1e204f848d641
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-07-10 03:04:32 -0400

    enhan: update kind from str type to Lexeme

--- geoips/models/v2alpha1/workflows.py
+++ geoips/models/v2alpha1/workflows.py
@@ -48,10 +48,7 @@ def get_plugin_names(plugin_kind: str) -> List[str]:
         If the plugin kind is invalid
 
     """
-    interface_name = plugin_kind
-
-    if not interface_name.endswith("s"):
-        interface_name = str(Lexeme(plugin_kind).plural)
+    interface_name = str(Lexeme(plugin_kind).plural)
 
     try:
         interface = getattr(interfaces, interface_name)
@@ -71,10 +68,7 @@ def get_plugin_kinds() -> set[str]:
         singular names of distinct plugin kinds
     """
     return {
-        # set comprehension
-        # the [:-1] slice converts the plugin kind from plural to singular
-        # eg. 'Readers' => 'Reader'
-        plugin_kinds[:-1]
+        str(Lexeme(plugin_kinds).singular)
         for ifs in interfaces.list_available_interfaces().values()
         for plugin_kinds in ifs
     }
@@ -164,7 +158,7 @@ class ReaderArgumentsModel(PermissiveFrozenModel):
 class WorkflowStepDefinitionModel(FrozenModel):
     """Validate step definition : kind, name, and arguments."""
 
-    kind: str = Field(..., description="plugin kind")
+    kind: Lexeme = Field(..., description="plugin kind")
     name: str = Field(..., description="plugin name", init=False)
     arguments: Dict[str, Any] = Field(default_factory=dict, description="step args")
 
