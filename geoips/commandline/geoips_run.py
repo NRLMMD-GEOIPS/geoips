ea052f89774ce8106202f740cc067c4c20e3c315
BSL-5 <biosafetylvl5@users.noreply.github.com>
2025-07-10 08:50:04 -0600

    Update geoips_run.py - blacken

--- geoips/commandline/geoips_run.py
+++ geoips/commandline/geoips_run.py
@@ -22,11 +22,15 @@ with import_optional_dependencies(loglevel="info"):
     # 'Failed to import data_fusion.commandline at /path/to/geoips/geoips/commandline/geoips_run.py:19. If you need it, install it.' # NOQA
     # every time we use the CLI. If a user needs the CLI, I'm assuming they'll know to
     # install it.
-    try:
+    from data_fusion.commandline.args import add_args as data_fusion_add_args
+
+    try: 
         from data_fusion.commandline.args import add_args as data_fusion_add_args
     except ModuleNotFoundError as e:
         try:
-            from geoips_data_fusion.commandline.args import add_args as data_fusion_add_args # noqa
+            from geoips_data_fusion.commandline.args import (
+                add_args as data_fusion_add_args,
+            )
         except ModuleNotFoundError:
             raise e
 
