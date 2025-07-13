3b069616f1f60aa9dd12138a3d6ca0b171a315fe
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-07-13 05:47:59 -0400

    sync with remote

---
+++ geoips/models/v1/__init__.py
@@ -0,0 +1,16 @@
+import importlib
+import pkgutil
+import inspect
+
+_modules = {}
+_classes = {}
+
+for _, modname, _ in pkgutil.iter_modules(__path__):
+    full_name = f"{__name__}.{modname}"
+    module = importlib.import_module(full_name)
+    _modules[full_name] = module
+
+    _classes[full_name] = [
+        name for name, obj in inspect.getmembers(module, inspect.isclass)
+        if obj.__module__ == full_name
+    ]
