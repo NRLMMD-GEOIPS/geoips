6d4463b0ebb5ff41cc64196c52efca91b18b424c
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-08-26 13:06:38 -0400

    docs: fix flake8 violation

--- geoips/models/__init__.py
+++ geoips/models/__init__.py
@@ -1,6 +1,6 @@
 # # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
-"""GeoIPS order-based procflow models init file"""
+"""GeoIPS order-based procflow models init file."""
 
 __all__ = ["v1", "v2alpha1"]
