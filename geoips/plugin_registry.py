fb0227a47cab1186074154c425ca11282e59fec2
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-09-10 17:03:27 +0000

    cleanup

--- geoips/plugin_registry.py
+++ geoips/plugin_registry.py
@@ -551,9 +551,7 @@ class PluginRegistry:
 
         if getattr(interface_obj, "use_pydantic", False):
             validated = self.load_plugin(plugin)
-            # return validated
-            validated_dump = validated.model_dump()
-            return validated_dump
+            return validated.model_dump()
         else:
             validated = interface_obj.validator.validate(plugin)
             return interface_obj._plugin_yaml_to_obj(name, validated)
