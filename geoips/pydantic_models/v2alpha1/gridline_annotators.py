76bf30344bfb0572ea888edcfb08953794bbd737
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-09-30 19:02:11 +0000

    style: fix Black formatting and Flake8 violations

--- geoips/pydantic_models/v2alpha1/gridline_annotators.py
+++ geoips/pydantic_models/v2alpha1/gridline_annotators.py
@@ -13,7 +13,11 @@ from typing import Literal, Union, List, Optional
 
 from pydantic import Field
 
-from geoips.pydantic_models.root_bases import ColorType, FrozenModel, PermissiveFrozenModel
+from geoips.pydantic_models.root_bases import (
+    ColorType,
+    FrozenModel,
+    PermissiveFrozenModel,
+)
 from geoips.pydantic_models.v2alpha1.bases import PluginModel
 
 
