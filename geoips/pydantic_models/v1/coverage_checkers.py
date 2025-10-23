1c7627c709c772ae2b6efefa407bc31617335d0f
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-10-23 23:47:47 +0000

    docs: add module docstrings for coverage-checker plugins

---
+++ geoips/pydantic_models/v1/coverage_checkers.py
@@ -0,0 +1,4 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+"""Pydantic models used to validate GeoIPS OBP v1 coverage-checker plugins."""
