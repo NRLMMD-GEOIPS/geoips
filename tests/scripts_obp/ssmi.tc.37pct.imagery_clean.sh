539424de4f6ba6a88c399f63ddb9fe9bb74cd953
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-10 21:10:34 +0000

    feat(obp): introduce OBP workflow runner script for ssmi.tc.37pct.imagery_clean product

---
+++ tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
@@ -0,0 +1,9 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+geoips run order_based -w   \
+          $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.sdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
+          $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.tdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
+retval=$?
+
+exit $retval
