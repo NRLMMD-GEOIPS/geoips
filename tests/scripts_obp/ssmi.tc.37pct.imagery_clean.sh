4a0ff3337aaf8193a60b830fa6d75aa6271154e6
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 04:08:05 +0000

    chore(cspell): add one-time ignore words

--- tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
+++ tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
@@ -1,6 +1,8 @@
 # # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
+# cspell:ignore bdecks cfnoc sdrmi tdrmi
+
 geoips run order_based -w   \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.sdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.tdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
