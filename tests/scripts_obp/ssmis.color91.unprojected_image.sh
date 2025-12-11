3c3e84b26278d7b5018eea6ef7ced741477e93df
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 05:26:54 +0000

    chore(cspell): add one-time ignore words

--- tests/scripts_obp/ssmis.color91.unprojected_image.sh
+++ tests/scripts_obp/ssmis.color91.unprojected_image.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 # # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
-# cspell:ignore bdecks cfnoc sdrmi tdrmi
+# cspell:ignore cfnoc sdris
 
 set -eu
 
