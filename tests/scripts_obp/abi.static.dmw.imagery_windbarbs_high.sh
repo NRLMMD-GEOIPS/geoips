629eb655e544b988a8e01842bce957a5241e4153
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 06:21:13 +0000

    enhanc: further cleanup

--- tests/scripts_obp/abi.static.dmw.imagery_windbarbs_high.sh
+++ tests/scripts_obp/abi.static.dmw.imagery_windbarbs_high.sh
@@ -1,23 +1,10 @@
 # # # This source code is subject to the license referenced at
 # # # https://github.com/NRLMMD-GEOIPS.
 
-# Default values - if you do not have this exact test case available, call with available data files / sectors.
-
-# This exact test case required for valid comparisons - remove "compare_path" argument if running a different
-# set of arguments.
-
 # cspell:ignore DMWVF
-geoips run order_based -w  $GEOIPS_TESTDATA_DIR/test_data_noaa_aws/data/goes16/20210929/0000/OR_ABI-L2-DMWVF*.nc \
-             --reader_name abi_l2_netcdf \
-            #  --product_name DMW-High \
-            #  --output_formatter imagery_windbarbs \
-            #  --filename_formatter geoips_fname \
-            #  --resampled_read \
-            #  --logging_level info \
-            #  --sector_list goes_east \
-            #  --minimum_coverage 0 \
-            #  --compare_path "$GEOIPS_PACKAGES_DIR/geoips/tests/outputs/abi.static.<product>.imagery_windbarbs" \
 
-retval=$?
+set -eu
 
-exit $retval
+geoips run order_based -f $GEOIPS_PACKAGES_DIR/geoips/tests/scripts_obp/abi.static.dmw.imagery_windbarbs_high.yaml $GEOIPS_TESTDATA_DIR/test_data_noaa_aws/data/goes16/20210929/0000/OR_ABI-L2-DMWVF*.nc
+
+exit $?
