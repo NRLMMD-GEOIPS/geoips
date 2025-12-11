4ced9866a589a8cb345b21276368bac518508a13
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 04:06:19 +0000

    refactor: simplify exit status handling

--- tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
+++ tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
@@ -4,6 +4,5 @@
 geoips run order_based -w   \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.sdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.tdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
-retval=$?
 
-exit $retval
+exit $?
