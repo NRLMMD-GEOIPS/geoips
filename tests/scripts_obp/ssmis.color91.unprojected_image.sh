10039cfc6151e5068e74d3f77fb2d25adde1a793
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 05:20:11 +0000

    feat(obp): introduce obp workflow and runner for SSMIS color91 product

---
+++ tests/scripts_obp/ssmis.color91.unprojected_image.sh
@@ -0,0 +1,12 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+# cspell:ignore bdecks cfnoc sdrmi tdrmi
+
+set -eu
+
+geoips run order_based -f $GEOIPS_PACKAGES_DIR/geoips/tests/scripts_obp/ssmis.color91.unprojected_image.sh   \
+          $GEOIPS_TESTDATA_DIR/test_data_ssmis/data/US058SORB-RAWspp.sdris_f16_d20200519_s084400_e102900_r85579_cfnoc.raw
+
+exit $?
+
