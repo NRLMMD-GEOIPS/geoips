c3d89bf7897cbdd44dc69612b383f474f730beec
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 04:47:12 +0000

    enhancement(obp-run): run workflow using the --filepath flag

--- tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
+++ tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh
@@ -3,7 +3,7 @@
 
 # cspell:ignore bdecks cfnoc sdrmi tdrmi
 
-geoips run order_based -w   \
+geoips run order_based -f $GEOIPS_PACKAGES_DIR/geoips/tests/scripts_obp/ssmi.tc.37pct.imagery_clean.sh   \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.sdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
           $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.tdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
 
