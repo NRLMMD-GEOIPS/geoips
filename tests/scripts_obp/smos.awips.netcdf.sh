59ada60bd08d99c9c4c2d98d6fc7e302dc9dd2e8
srikanth-kumar <srikanthkumargampa@gmail.com>
2025-12-11 06:31:27 +0000

    feat(obp): introduce OBP workflow and runner script for smos.awips product

---
+++ tests/scripts_obp/smos.awips.netcdf.sh
@@ -0,0 +1,9 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+set -eu
+
+geoips run order_based -f $GEOIPS_PACKAGES_DIR/geoips/tests/scripts_obp/smos.awips.netcdf.yaml   \
+          $GEOIPS_TESTDATA_DIR/test_data_ssmi/data/US058SORB-DEFspp.sdrmi_f15_d20200519_s080800_e095300_r05633_cfnoc.def \
+
+exit $?
