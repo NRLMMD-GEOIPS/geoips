8297465a7b89bc54a770dc357006188e31cb6116
coleman-m <coleman.mcclelland@colostate.edu>
2026-02-27 12:39:15 -0700

    Beginning work on this model validator

--- geoips/pydantic_models/v1/output_checkers.py
+++ geoips/pydantic_models/v1/output_checkers.py
@@ -5,9 +5,11 @@
 
 from typing import List, Optional
 
-from pydantic import Field
+from pydantic import Field, model_validator
 
 from geoips.pydantic_models.v1.bases import FrozenModel
+
+from ipdb import set_trace as shell
 
 
 class OutputCheckersArgumentsModel(FrozenModel):
@@ -25,3 +27,28 @@ class OutputCheckersArgumentsModel(FrozenModel):
         strict=True,
         description="A list of paths to the output file(s).",
     )
+
+    @model_validator(mode="after")
+    def _validate_output_checker_path(
+        cls, model: OutputCheckersArgumentsModel
+    ) -> OutputCheckersArgumentsModel:
+        """
+        Ensure the Output Checker contains a compare_path if checker_name is present.
+
+        Parameters
+        ----------
+        model: OutputCheckersArgumentsModel
+            The OutputCheckersArgumentsModel instance to validate.
+
+        Returns
+        -------
+        OutputCheckersArgumentsModel
+            The validated instance of OutputCheckersArgumentsModel
+
+        Raises
+        ------
+        ValueError
+            If the plugin name is not valid for the specified plugin kind.
+        """
+        shell()
+        return model
