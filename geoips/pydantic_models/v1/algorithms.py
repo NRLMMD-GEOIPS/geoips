d9c1b902c7886add9f6fda25a15aa847815dacf0
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-03-09 10:48:45 +0000

    feat: add new fields to AlgorithmArgumentsModel

--- geoips/pydantic_models/v1/algorithms.py
+++ geoips/pydantic_models/v1/algorithms.py
@@ -23,3 +23,13 @@ class AlgorithmArgumentsModel(PermissiveFrozenModel):
     max_outbounds: str = Field("mask")
     norm: StrictBool = Field(False)
     inverse: StrictBool = Field(False)
+    # This should default to (?, 1000) or (1000, ?) or (None, None)
+    pressure_level_range: int = Field()
+    pressure_key: str = Field(None)
+    time_key: str = Field(...)
+    time_fcst: int = Field(-1)
+    # verify if the type should be string
+    time_dim: str = Field(None)
+    grid_geo: bool = Field(False)
+    var_map: str = Field(None)
+
