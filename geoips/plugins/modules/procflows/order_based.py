873d8eb192968998fc7b1823899a151b833ffcae
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-03-16 06:09:50 +0000

    feat: add 'algorithms' to list of handled_interfaces

--- geoips/plugins/modules/procflows/order_based.py
+++ geoips/plugins/modules/procflows/order_based.py
@@ -37,7 +37,7 @@ def call(workflow, fnames, command_line_args=None):
     LOG.interactive(f"Begin processing '{workflow['name']}' workflow.")
     wf_plugin = workflow
 
-    handled_interfaces = ["readers", "coverage_checkers"]
+    handled_interfaces = ["readers", "coverage_checkers", "algorithms"]
     for step_id, step_def in wf_plugin["spec"]["steps"].items():
         interface = str(Lexeme(step_def["kind"]).plural)
 
