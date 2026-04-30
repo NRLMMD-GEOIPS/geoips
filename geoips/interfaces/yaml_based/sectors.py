0059cca830fd671491941f9e0141b539d2837adf
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-04-30 09:47:19 +0000

    feat(sectors): enable pydantic validation for sector plugins

--- geoips/interfaces/yaml_based/sectors.py
+++ geoips/interfaces/yaml_based/sectors.py
@@ -158,8 +158,7 @@ class SectorsInterface(BaseYamlInterface):
 
     name = "sectors"
     plugin_class = SectorPluginBase
-    use_pydantic = gpaths["GEOIPS_USE_PYDANTIC"]
-
+    use_pydantic = True
     # if sectors.get_plugin(<name>) is found to be a dynamic sector. Otherwise, a static
     # sector plugin model (I.e. SectorPluginModel) will be used for all other sector
     # types.
