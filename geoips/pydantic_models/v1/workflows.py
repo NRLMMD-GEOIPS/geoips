08c6e2f0274567c1a05c98d77adc4755990f1691
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-05-11 20:51:25 +0000

    chore: resovle merge conflicts with main

--- geoips/pydantic_models/v1/workflows.py
+++ geoips/pydantic_models/v1/workflows.py
@@ -203,50 +203,6 @@ class GlobalVariablesModel(PermissiveFrozenModel):
         return model
 
 
-class ReaderArgumentsModel(PermissiveFrozenModel):
-    """Reader step argument definition.
-
-    Pydantic model defining and validating Reader step arguments.
-    """
-
-    area_def: str = Field(None, description="Area definition identifier.")
-    variables: List[str] = Field(
-        None,
-        description="List of channels to process",
-        alias="chans",
-    )
-    metadata_only: bool = Field(False, description="Read metadata only.")
-    self_register: List[str] = Field(None, description="Enable self-registration.")
-    fnames: List[str] = Field(
-        None, description="full path to the file(s) for static dataset inputs."
-    )
-
-    @model_validator(mode="before")
-    def _handle_deprecated_chans(cls, values):
-        """
-        Check for the deprecated 'chans' field and issue a warning.
-
-        This method detects if `chans` is present in the input values and issues a
-        deprecation warning, recommending the use of 'variable' instead.
-
-        Parameters
-        ----------
-        values : dict
-            Input values to the model.
-
-        Returns
-        -------
-        dict
-            The original input values.
-        """
-        if "chans" in values:
-            LOG.warning(
-                "'chans' is deprecated and will be removed in GeoIPS 2.0. Use"
-                "'variables' instead."
-            )
-        return values
-
-
 class WorkflowStepDefinitionModel(FrozenModel):
     """Validate step definition : kind, name, and arguments."""
 
