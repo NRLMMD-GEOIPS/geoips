a0da37561c9cf97acb26f1da38ed2f23cfd10e23
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-05-15 22:47:30 +0000

    clean up

--- geoips/pydantic_models/v1/filename_formatters.py
+++ geoips/pydantic_models/v1/filename_formatters.py
@@ -16,24 +16,17 @@ class FilenameFormatterArgumentsModel(PermissiveFrozenModel):
     Pydantic model defining and validating Filename Formatter step arguments.
     """
 
-    # product_names is moved to GlobalVariables
-    # coverage should go to GlobalVariables
     coverage: float = Field(
         None, description="Image coverage, float between 0.0 and 100.0"
     )
     output_type: str = Field(
         None,
     )
-    # What does default to output_type means here
     output_type_dir: DirectoryPath = Field(
         None, description="If None, default to output_type."
     )
     product_dir: DirectoryPath = Field(None)
-    # product_dir isNone then set product_dir = product_name
-    # Write a model validator for this
     product_subdir: DirectoryPath = Field(None)
-    # if this is None then set
-    # source_dir = source_name
     source_dir: DirectoryPath = Field(None)
     basedir: DirectoryPath = Field(None, description="Base directory for output file.")
     output_dict: DirectoryPath = Field(None)
