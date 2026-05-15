e4b659641c730792b6b35a4f27e31bce42db07e0
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-05-15 22:41:49 +0000

    style: fix Black formatting and Flake8 violation issues

--- geoips/pydantic_models/v1/filename_formatters.py
+++ geoips/pydantic_models/v1/filename_formatters.py
@@ -4,7 +4,7 @@
 """Pydantic models used to validate GeoIPS OBP v1 filename-formatter plugins."""
 
 # Third-Party Libraries
-from pydantic import  DirectoryPath, Field
+from pydantic import DirectoryPath, Field
 
 # GeoIPS imports
 from geoips.pydantic_models.v1.bases import PermissiveFrozenModel
@@ -15,21 +15,25 @@ class FilenameFormatterArgumentsModel(PermissiveFrozenModel):
 
     Pydantic model defining and validating Filename Formatter step arguments.
     """
+
     # product_names is moved to GlobalVariables
     # coverage should go to GlobalVariables
-    # 
-    coverage: float = Field(None, description="Image coverage, float between 0.0 and 100.0")
-    output_type: str = Field(None, )
+    coverage: float = Field(
+        None, description="Image coverage, float between 0.0 and 100.0"
+    )
+    output_type: str = Field(
+        None,
+    )
     # What does default to output_type means here
-    output_type_dir: DirectoryPath = Field(None, description="If None, default to output_type.")
+    output_type_dir: DirectoryPath = Field(
+        None, description="If None, default to output_type."
+    )
     product_dir: DirectoryPath = Field(None)
-    #product_dir isNone then set product_dir = product_name
-    # Write a model validator for this 
+    # product_dir isNone then set product_dir = product_name
+    # Write a model validator for this
     product_subdir: DirectoryPath = Field(None)
     # if this is None then set
     # source_dir = source_name
     source_dir: DirectoryPath = Field(None)
-    basedir:  DirectoryPath = Field(None, description="Base directory for output file.")
+    basedir: DirectoryPath = Field(None, description="Base directory for output file.")
     output_dict: DirectoryPath = Field(None)
-
-
