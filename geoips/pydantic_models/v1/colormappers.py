47109d5bb60e1bfa25b2029ba54c2bbd1a443fd3
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-05-26 17:52:50 +0000

    feat: add create_colorbar field

--- geoips/pydantic_models/v1/colormappers.py
+++ geoips/pydantic_models/v1/colormappers.py
@@ -19,37 +19,20 @@ class ColormapperArgumentsModel(PermissiveFrozenModel):
     Pydantic model defining and validating Colormapper step arguments.
     """
 
-    data_range: tuple[float, float] = Field(
-        Plugin_provided, description="Min and max value for colormap"
-    )
-    cbal_lavel: str = Field(Plugin_provided, description="")
-    pressure_range_legend: List[str] = Field(
+    cbar_label: str = Field(
         None,
-        description="List of strings that are used for setting the cbar tick labels",
+        description=(
+            "Positional parameter passed to cbar.set_label"
+            " If specified, use cbar_label string as colorbar label."
+        ),
     )
-    data_range = (None,)
-    cmap_name = ("Greys",)
-    cmap_source = ("matplotlib",)
-    cmap_path = (None,)
-    create_colorbar = (True,)
-    cbar_label = (None,)
-    cbar_ticks = (None,)
-    cbar_tick_labels = (None,)
-    cbar_spacing: str = Field(
-        "proportional",
-        description='"spacing" argument to pass to fig.colorbar; can also specify directly within "colorbar_kwargs"',
+    cmap_name: str = Field(
+        "Greys",
+        description=(
+            "Specify the name of the resulting matplotlib colormap. If no ascii_path"
+            " specified, will use builtin matplotlib colormap of name cmap_name."
+        ),
     )
-    cbar_full_width: bool = Field(
-        True, description='"Extend the colorbar across the full width of the image"'
-    )
-    colorbar_kwargs: dict = Field(
-        None, description='keyword arguments to pass through directly to "fig.colorbar"'
-    )
-    set_ticks_kwargs: dict = Field(
-        None,
-        description='keyword arguments to pass through directly to "cbar.set_ticks"',
-    )
-    set_label_kwarg: dict = Field(
-        None,
-        description='keyword arguments to pass through directly to "cbar.set_label"',
+    create_colorbar: bool = Field(
+        True, description="Specify whether the image should contain a colorbar or not."
     )
