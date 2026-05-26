caa3189f74d972bd0569d6c046ab29d47e567536
srikanth-kumar <srikanthkumargampa@gmail.com>
2026-05-26 17:28:57 +0000

    style: Black formatting changes

--- geoips/pydantic_models/v1/colormappers.py
+++ geoips/pydantic_models/v1/colormappers.py
@@ -3,7 +3,6 @@
 
 """Pydantic models used to validate GeoIPS OBP v1 colormapper plugins."""
 
-
 # Python Standard Libraries
 from typing import List
 
@@ -20,19 +19,37 @@ class ColormapperArgumentsModel(PermissiveFrozenModel):
     Pydantic model defining and validating Colormapper step arguments.
     """
 
-    data_range: tuple[float, float] = Field(Plugin_provided, description="Min and max value for colormap")
-    cbal_lavel:str = Field(Plugin_provided, description="")
-    pressure_range_legend: List[str] = Field(None, description="List of strings that are used for setting the cbar tick labels")
-    data_range=None,
-    cmap_name="Greys",
-    cmap_source="matplotlib",
-    cmap_path=None,
-    create_colorbar=True,
-    cbar_label=None,
-    cbar_ticks=None,
-    cbar_tick_labels=None,
-    cbar_spacing: str = Field("proportional", description='"spacing" argument to pass to fig.colorbar; can also specify directly within "colorbar_kwargs"')
-    cbar_full_width: bool = Field(True, description='"Extend the colorbar across the full width of the image"')
-    colorbar_kwargs: dict = Field(None, description='keyword arguments to pass through directly to "fig.colorbar"')
-    set_ticks_kwargs: dict = Field(None, description='keyword arguments to pass through directly to "cbar.set_ticks"')
-    set_label_kwarg: dict = Field(None, description='keyword arguments to pass through directly to "cbar.set_label"')
+    data_range: tuple[float, float] = Field(
+        Plugin_provided, description="Min and max value for colormap"
+    )
+    cbal_lavel: str = Field(Plugin_provided, description="")
+    pressure_range_legend: List[str] = Field(
+        None,
+        description="List of strings that are used for setting the cbar tick labels",
+    )
+    data_range = (None,)
+    cmap_name = ("Greys",)
+    cmap_source = ("matplotlib",)
+    cmap_path = (None,)
+    create_colorbar = (True,)
+    cbar_label = (None,)
+    cbar_ticks = (None,)
+    cbar_tick_labels = (None,)
+    cbar_spacing: str = Field(
+        "proportional",
+        description='"spacing" argument to pass to fig.colorbar; can also specify directly within "colorbar_kwargs"',
+    )
+    cbar_full_width: bool = Field(
+        True, description='"Extend the colorbar across the full width of the image"'
+    )
+    colorbar_kwargs: dict = Field(
+        None, description='keyword arguments to pass through directly to "fig.colorbar"'
+    )
+    set_ticks_kwargs: dict = Field(
+        None,
+        description='keyword arguments to pass through directly to "cbar.set_ticks"',
+    )
+    set_label_kwarg: dict = Field(
+        None,
+        description='keyword arguments to pass through directly to "cbar.set_label"',
+    )
